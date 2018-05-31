02eccc52f9b0044a3beed0ef8bec99ebf3977a30
Elad Ben-Israel <benisrae@amazon.com>
2018-05-31 12:42:39 +0300

    Improve pack.sh
    Use "lerna exec" to ensure we run pack in a lerna environment.

--- pack.sh
+++ pack.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 set -e
-modules="$(node_modules/.bin/lerna ls 2>/dev/null | grep -v private | cut -d' ' -f1 | xargs)"
-version="$(cat lerna.json | grep version | cut -d'"' -f4)"
+export PATH=$PWD/node_modules/.bin:$PATH
 
-echo "Packing version ${version}..."
-
-root="$PWD"
-dist="${root}/dist"
-
+dist="$PWD/dist"
 rm -fr ${dist}
 mkdir -p ${dist}
-cd ${dist}
 
-for module in ${modules}; do
-    npm pack ${root}/packages/${module}
-done
-
-echo "Tarballs under: ${dist}"
+lerna ls | grep -v "private" | cut -d" " -f1 | xargs -n1 -I{} \
+    lerna exec --scope {} --stream -- "npm pack && mv *.tgz ${dist}"
