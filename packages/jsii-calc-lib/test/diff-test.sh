521980c23bef235b3b0940de42e562a053bea148
Elad Ben-Israel <benisrae@amazon.com>
2018-06-06 02:41:28 +0300

    Generate version of expected calc assemblies based on repo version
    Since in CI/CD we are creating a version that contains the commit hash
    we need the expected assembly versions to correspond to it. This will also
    ensure that when we bump MV, these expectations are automatically updated.

--- packages/jsii-calc-lib/test/diff-test.sh
+++ packages/jsii-calc-lib/test/diff-test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 cd $(dirname $0)
+
+echo | node <<HERE
+const fs = require('fs');
+const jsii = require('./jsii.json');
+const version = require('../package.json').version;
+jsii.version = version;
+fs.writeFileSync('./jsii.json', JSON.stringify(jsii, undefined, 2));
+HERE
+
+#node -e "require('./jsii.json')"
+
 diff ../dist/jsii.json jsii.json || {
   echo
   echo "========================================================================="
--- packages/jsii-calc/test/diff-test.sh
+++ packages/jsii-calc/test/diff-test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd $(dirname $0)
+
+echo | node <<HERE
+const fs = require('fs');
+const jsii = require('./jsii.json');
+const version = require('../package.json').version;
+jsii.version = version;
+jsii.dependencies['jsii\$jsii_calc_lib\$'].version = version;
+fs.writeFileSync('./jsii.json', JSON.stringify(jsii, undefined, 2));
+HERE
+
 diff ../dist/jsii.json jsii.json || {
   echo
   echo "========================================================================="
