5ad4f74cb699db2faea6a18c8ab5699e92a707c6
Elad Ben-Israel <benisrae@amazon.com>
2018-07-11 03:48:02 -0700

    Improve pacmak test robustness around tgz diffs (#77)
    Indicate that the checked-in .tgz are just placeholders and
    prepare the expectation directory in a staging location.
    
    Also, if a test fails, continue to next tests, so we don't
    have to re-run the entire thing for every failure.

--- packages/jsii-pacmak/test/diff-test.sh
+++ packages/jsii-pacmak/test/diff-test.sh
@@ -3,6 +3,14 @@ set -e
 cd $(dirname $0)
 
 langs=$((cd ../lib/generators && ls -1 *.d.ts) | sed -e 's/\.d\.ts//')
+workdir="$(mktemp -d)"
+success=true
+
+function mktmpdir() {
+    local newdir="${workdir}/$RANDOM"
+    mkdir -p ${newdir}
+    echo ${newdir}
+}
 
 function assert-generator() {
     local module=$1
@@ -13,25 +21,38 @@ function assert-generator() {
     echo "=========================================================================================="
 
     # creating reference tgz for module ${module} (to avoid binary diffs across platforms)...
-    local tarball="${module_root}/$(cd ${module_root} && npm pack --ignore-scripts 2>/dev/null)"
-    local workdir=$(mktemp -d)
-    mv ${tarball} ${workdir}
-    local tarball="${workdir}/$(basename ${tarball})"
+    local expected_tarball="${module_root}/$(cd ${module_root} && npm pack --ignore-scripts 2>/dev/null)"
+    local workdir=$(mktmpdir)
+    mv ${expected_tarball} ${workdir}
+    local expected_tarball="${workdir}/$(basename ${expected_tarball})"
 
     for lang in $langs; do
-        local outdir="$(mktemp -d)"
-        local expected="$PWD/expected.${lang}.${module}"
+        local outdir="$(mktmpdir)"
+        local original_expected="$PWD/expected.${lang}.${module}"
+        local expected="$(mktmpdir)"
 
-        local expected_tarball=$(find ${expected} -name "*.tgz" || true)
-        if [ -n "${expected_tarball}" ]; then
-            rm -f ${expected_tarball} && cp ${tarball} ${expected_tarball}
+        if [ -d ${original_expected} ]; then
+            rsync -a ${original_expected}/ ${expected}/
+        fi
+
+        # put the real expected tarball instead of the placeholder
+        local expected_tarball_placeholder="$(find ${expected} -name "*.tgz" || true)"
+        if [ -n "${expected_tarball_placeholder}" ]; then
+            rm -f ${expected_tarball_placeholder} && cp ${expected_tarball} ${expected_tarball_placeholder}
         fi
 
         echo
         echo "Running jsii-pacmak for language ${lang}"
         echo "    Actual: ${outdir}"
-        echo "    Expected: ${expected}"
+        echo "    Expected: ${original_expected}"
         ../bin/jsii-pacmak -t ${lang} -o ${outdir} ${module_root}
+
+        # change the placeholder back
+        if [ -n "${expected_tarball_placeholder}" ]; then
+            local original_expected_tarball_placeholder="$(find ${original_expected} -name "*.tgz")"
+            echo "Placeholder for the expected module tarball"                              > ${original_expected_tarball_placeholder}
+            echo "The diff-test.sh harness will replace it with the real expected tarball" >> ${original_expected_tarball_placeholder}
+        fi
 
         if ! diff -arq ${outdir} ${expected}; then
             echo
@@ -39,9 +60,9 @@ function assert-generator() {
             echo " diff-test for pacmak generator ${lang}/${module} failed"
             echo "------------------------------------------------------------------------"
             echo " To update expectation run:"
-            echo "    rsync -av --delete ${outdir}/ ${expected}/"
+            echo "    rsync -av --delete ${outdir}/ ${original_expected}/"
             echo "------------------------------------------------------------------------"
-            exit 1
+            success=false
         fi
     done
 
@@ -52,3 +73,12 @@ function assert-generator() {
 
 assert-generator jsii-calc
 assert-generator jsii-calc-lib
+
+if ${success}; then
+    # only remove working directory if tests pass. Otherwise, user might want to
+    # update their expectations.
+    rm -fr "${workdir}"
+else
+    echo "SOME TESTS FAILED"
+fi
+
