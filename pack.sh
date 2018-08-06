3dcdb87517600a46f2e0e46319688791004ad44d
Elad Ben-Israel <benisrae@amazon.com>
2018-08-06 14:16:40 +0300

    Debug

--- pack.sh
+++ pack.sh
@@ -8,7 +8,7 @@ mkdir -p ${packdir}
 
 # run "npm run package" in all modules (which support it)
 # this should emit a publishable to the "dist" directory of each module
-lerna run package
+lerna run package --stream
 
 # collect all "dist" directories into "pack"
 for dist in $(lerna exec "[ -d ./dist ] && echo \${PWD}/dist || true"); do
