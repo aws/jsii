a1cbd23c09b1f17701d684844bcafa54779c7a9a
Matthew Pirocchi <pirocchi@amazon.com>
2018-08-31 12:06:09 -0700

    Pass in SNK region as environment variable instead of hard-coding.

--- fetch-dotnet-snk.sh
+++ fetch-dotnet-snk.sh
@@ -7,12 +7,12 @@ echo "Retrieving SNK..."
 
 sudo apt install jq -y
 
-ROLE=$(aws sts assume-role --region us-east-2 --role-arn ${DOTNET_STRONG_NAME_ROLE_ARN:-} --role-session-name "jsii-dotnet-snk")
+ROLE=$(aws sts assume-role --region ${{DOTNET_STRONG_NAME_SECRET_REGION:-} --role-arn ${DOTNET_STRONG_NAME_ROLE_ARN:-} --role-session-name "jsii-dotnet-snk")
 export AWS_ACCESS_KEY_ID=$(echo $ROLE | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $ROLE | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $ROLE | jq .Credentials.SessionToken)
 
-SNK_SECRET=$(aws secretsmanager get-secret-value --region us-east-2 --secret-id ${DOTNET_STRONG_NAME_SECRET_ID:-})
+SNK_SECRET=$(aws secretsmanager get-secret-value --region ${{DOTNET_STRONG_NAME_SECRET_REGION:-} --secret-id ${DOTNET_STRONG_NAME_SECRET_ID:-})
 TMP_DIR=$(mktemp -d)
 TMP_KEY="$TMP_DIR/key.snk"
 echo $SNK_SECRET | jq -r .SecretBinary | base64 --decode > $TMP_KEY
