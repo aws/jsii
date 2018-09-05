fa291d77f9fecf88a904bc11bbbf111424d4aabe
Donald Stufft <donald@stufft.io>
2018-09-05 16:55:47 -0400

    Bump versions

--- packages/jsii-python-runtime/src/jsii/kernel/providers/process.py
+++ packages/jsii-python-runtime/src/jsii/kernel/providers/process.py
@@ -149,7 +149,7 @@ class _NodeProcess:
 
         # TODO: Replace with proper error.
         assert (
-            resp.hello == "jsii-runtime@0.6.4"
+            resp.hello == "jsii-runtime@0.7.1"
         ), f"Invalid JSII Runtime Version: {resp.hello!r}"
 
     def send(
