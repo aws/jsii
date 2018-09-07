cf62773632e8079d630548bc416b588bcbe161c5
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2018-09-07 16:42:21 -0400

    Force generated assemblies to reference jsii-only dependencies. (#216)

---
+++ packages/jsii-pacmak/test/expected.jsii-calc-base/dotnet/Amazon.JSII.Tests.CalculatorPackageId.BasePackageId/Amazon/JSII/Tests/CalculatorNamespace/BaseNamespace/Internal/DependencyResolution/Anchor.cs
@@ -0,0 +1,10 @@
+namespace Amazon.JSII.Tests.CalculatorNamespace.BaseNamespace.Internal.DependencyResolution
+{
+    public class Anchor
+    {
+        public Anchor()
+        {
+            new Amazon.JSII.Tests.CalculatorNamespace.BaseOfBaseNamespace.Internal.DependencyResolution.Anchor();
+        }
+    }
+}
