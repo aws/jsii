45fa8580a775b6f0a0bc779dba4904321e3d2988
Elad Ben-Israel <benisrae@amazon.com>
2018-09-13 21:42:44 +0300

    chore: prepare jsii for automatic publishing (#225)
    1. Remove the bundle stage. It is done now by the GitHub Releases publisher
    2. Emit all files to dist/
    3. Create a build.json with metadata for GH releases
    4. Copy CHANGELOG.md to dist/ for GH release notes

--- pack.sh
+++ pack.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# The purpose of this script is to prepare a "dist" directory
+# according to the conventions of the Buildable pipeline, so
+# that artifacts can be published to the various package managers
 set -e
 export PATH=$PWD/node_modules/.bin:$PATH
 
-packdir="$PWD/pack"
-rm -fr ${packdir}
-mkdir -p ${packdir}
+distdir="$PWD/dist"
+rm -fr ${distdir}
+mkdir -p ${distdir}
 
 # run "npm run package" in all modules (which support it)
 # this should emit a publishable to the "dist" directory of each module
@@ -13,6 +16,20 @@ lerna run package --stream
 # collect all "dist" directories into "pack"
 for dist in $(lerna exec "[ -d ./dist ] && echo \${PWD}/dist || true"); do
   echo "collecting ${dist}..."
-  rsync -av ${dist}/ ${packdir}/
+  rsync -av ${dist}/ ${distdir}/
 done
 
+# create a build.json file with build metadata
+version="$(node -e "console.log(require('./lerna.json').version)")"
+cat > ${distdir}/build.json <<HERE
+{
+  "name": "jsii",
+  "version": "${version}"
+}
+HERE
+
+# copy CHANGELOG.md to dist/ for github releases
+cp CHANGELOG.md ${distdir}/
+
+# for posterity, print all files in dist
+find dist/
