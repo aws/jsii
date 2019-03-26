9186370ba91f5da2dfa02e7def6678436c4c4dc6
Donald Stufft <donald@stufft.io>
2019-03-26 15:29:39 -0400

    Fixup metadata generation

--- packages/jsii-python-runtime/setup.py
+++ packages/jsii-python-runtime/setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    author=metadata["author"]["name"],
+    author=metadata["author"],
 
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
