c81b4c180e7b64acf16a5c423e5ab88203a7496c
Romain Marcadier-Muller <romain.muller@telecomnancy.net>
2019-04-16 10:49:13 +0200

    fix(kernel): Set `this` in static contexts (#460)
    Javascript allows static members to refer to other static members
    through `this`, but the `jsii-kernel` passed `null` for the `this`
    context in `sinvoke`, making it impossible to invoke static methods that
    made use of this feature through the `jsii` runtimes.
    
    Fixes awslabs/aws-cdk#2304

---
+++ packages/jsii-pacmak/test/expected.jsii-calc/java/src/main/java/software/amazon/jsii/tests/calculator/StaticContext.java
@@ -0,0 +1,26 @@
+package software.amazon.jsii.tests.calculator;
+
+/**
+ * This is used to validate the ability to use `this` from within a static context.
+ * 
+ * https://github.com/awslabs/aws-cdk/issues/2304
+ */
+@javax.annotation.Generated(value = "jsii-pacmak")
+@software.amazon.jsii.Jsii(module = software.amazon.jsii.tests.calculator.$Module.class, fqn = "jsii-calc.StaticContext")
+public class StaticContext extends software.amazon.jsii.JsiiObject {
+    protected StaticContext(final software.amazon.jsii.JsiiObject.InitializationMode mode) {
+        super(mode);
+    }
+
+    public static java.lang.Boolean canAccessStaticContext() {
+        return software.amazon.jsii.JsiiObject.jsiiStaticCall(software.amazon.jsii.tests.calculator.StaticContext.class, "canAccessStaticContext", java.lang.Boolean.class);
+    }
+
+    public static java.lang.Boolean getStaticVariable() {
+        return software.amazon.jsii.JsiiObject.jsiiStaticGet(software.amazon.jsii.tests.calculator.StaticContext.class, "staticVariable", java.lang.Boolean.class);
+    }
+
+    public static void setStaticVariable(final java.lang.Boolean value) {
+        software.amazon.jsii.JsiiObject.jsiiStaticSet(software.amazon.jsii.tests.calculator.StaticContext.class, "staticVariable", java.util.Objects.requireNonNull(value, "staticVariable is required"));
+    }
+}
