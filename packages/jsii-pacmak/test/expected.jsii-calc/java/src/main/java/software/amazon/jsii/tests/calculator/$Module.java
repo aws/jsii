3ce053477358c9ce79a79e424a35e50beaf4578f
Rico Huijbers <rix0rrr@gmail.com>
2019-04-18 14:37:55 +0200

    v0.10.2 (#472)
    See CHANGELOG

--- packages/jsii-pacmak/test/expected.jsii-calc/java/src/main/java/software/amazon/jsii/tests/calculator/$Module.java
+++ packages/jsii-pacmak/test/expected.jsii-calc/java/src/main/java/software/amazon/jsii/tests/calculator/$Module.java
@@ -7,7 +7,7 @@ import software.amazon.jsii.JsiiModule;
 
 public final class $Module extends JsiiModule {
     public $Module() {
-        super("jsii-calc", "0.10.1", $Module.class, "jsii-calc@0.10.1.jsii.tgz");
+        super("jsii-calc", "0.10.2", $Module.class, "jsii-calc@0.10.2.jsii.tgz");
     }
 
     @Override
