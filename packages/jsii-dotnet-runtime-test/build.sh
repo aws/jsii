10e2bfe7dde6d5e0a6f5a0618c77be44d129c491
Romain Marcadier-Muller <rmuller@amazon.com>
2019-06-03 14:36:54 +0200

    feat(jsii-spec): Add optional metadata field (#512)
    This can be used to track free-form maintainer data, for use in systems that
    are not part of the standard JSII toolchain. For example, it can be used to
    encode configuration behavior for proprietary tools that process assemblies.

--- packages/jsii-dotnet-runtime-test/build.sh
+++ packages/jsii-dotnet-runtime-test/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-# TODO: Auto-rev NuGet package versions on each local build.
-# Because we we don't rev the versions, dotnet will pick
-# up an old build from the cache if it exists. So we
-# explicitly clear the cache as a temporary workaround.
-dotnet nuget locals all --clear
-dotnet build -c Release ./test/Amazon.JSII.Runtime.IntegrationTests
+dotnet build --force -c Release ./test/Amazon.JSII.Runtime.IntegrationTests
