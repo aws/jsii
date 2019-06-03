a01ffa94e5c04bf6a0a85a3fdf5f1934e2c90b80
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-06-03 14:24:41 +0200

    Fix test expectations

--- packages/jsii-dotnet-jsonmodel/build.sh
+++ packages/jsii-dotnet-jsonmodel/build.sh
@@ -3,11 +3,6 @@ set -euo pipefail
 
 npm run gen
 
-# TODO: Auto-rev NuGet package versions on each local build.
-# Because we we don't rev the versions, dotnet will pick
-# up an old build from the cache if it exists. So we
-# explicitly clear the cache as a temporary workaround.
-dotnet nuget locals all --clear
-dotnet build -c Release ./src/Amazon.JSII.JsonModel.sln
+dotnet build --force -c Release ./src/Amazon.JSII.JsonModel.sln
 
 cp -f ./bin/Release/NuGet/*.nupkg .
--- packages/jsii-dotnet-runtime/build.sh
+++ packages/jsii-dotnet-runtime/build.sh
@@ -6,15 +6,9 @@ bundle_dir="src/Amazon.JSII.Runtime/jsii-runtime"
 mkdir -p ${bundle_dir}
 rsync -av node_modules/jsii-runtime/webpack/ ${bundle_dir}
 
-# TODO: Auto-rev NuGet package versions on each local build.
-# Because we we don't rev the versions, dotnet will pick
-# up an old build from the cache if it exists. So we
-# explicitly clear the cache as a temporary workaround.
-dotnet nuget locals all --clear
-
 # Build just Runtime and it's dependencies instead of the
 # solution to avoid integration tests from trying to be
 # built before the calc packages are generated.
-dotnet build -c Release ./src/Amazon.JSII.Runtime
+dotnet build --force -c Release ./src/Amazon.JSII.Runtime
 
 cp -f ./bin/Release/NuGet/*.nupkg .
