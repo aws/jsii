7019623b60170180d9c75d83f91b8f381a0fe6ab
Rico Huijbers <rix0rrr@gmail.com>
2019-07-01 17:44:37 +0200

    v0.13.3
    See CHANGELOG

--- packages/jsii-pacmak/test/expected.jsii-calc-base/dotnet/Amazon.JSII.Tests.CalculatorPackageId.BasePackageId/AssemblyInfo.cs
+++ packages/jsii-pacmak/test/expected.jsii-calc-base/dotnet/Amazon.JSII.Tests.CalculatorPackageId.BasePackageId/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using Amazon.JSII.Runtime.Deputy;
 
-[assembly: JsiiAssembly("@scope/jsii-calc-base", "0.13.2", "scope-jsii-calc-base-0.13.2.tgz")]
+[assembly: JsiiAssembly("@scope/jsii-calc-base", "0.13.3", "scope-jsii-calc-base-0.13.3.tgz")]
--- packages/jsii-pacmak/test/expected.jsii-calc-base/java/src/main/java/software/amazon/jsii/tests/calculator/base/$Module.java
+++ packages/jsii-pacmak/test/expected.jsii-calc-base/java/src/main/java/software/amazon/jsii/tests/calculator/base/$Module.java
@@ -7,7 +7,7 @@ import software.amazon.jsii.JsiiModule;
 
 public final class $Module extends JsiiModule {
     public $Module() {
-        super("@scope/jsii-calc-base", "0.13.2", $Module.class, "jsii-calc-base@0.13.2.jsii.tgz");
+        super("@scope/jsii-calc-base", "0.13.3", $Module.class, "jsii-calc-base@0.13.3.jsii.tgz");
     }
 
     @Override
