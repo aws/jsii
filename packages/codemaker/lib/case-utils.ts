c84fc7df876e0b70b0b35504047c7a3eb43ca742
Rico Huijbers <rix0rrr@gmail.com>
2019-07-07 11:32:52 +0200

    fix(python): respect mixed-case abbrevs while snake-casing (#588)
    Common mixed-case abbrevations will be snake-cased as a while, instead
    of being broken up into separate word parts.

--- packages/codemaker/lib/case-utils.ts
+++ packages/codemaker/lib/case-utils.ts
@@ -1,6 +1,12 @@
 import { default as camelcase } from 'camelcase';
 import decamelize = require('decamelize');
 
+const COMMON_ABBREVIATIONS = [
+    'KiB',
+    'MiB',
+    'GiB',
+];
+
 export function toCamelCase(...args: string[]) {
     return camelcase(args);
 }
@@ -10,5 +16,17 @@ export function toPascalCase(...args: string[]) {
 }
 
 export function toSnakeCase(s: string, sep = '_') {
+    // Save common abbrevations
+    s = s.replace(ABBREV_RE, (_, before, abbr, after) => before + ucfirst(abbr.toLowerCase()) + after);
     return decamelize(s, sep);
+}
+
+function regexQuote(s: string) {
+    return s.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&");
+}
+
+const ABBREV_RE = new RegExp('(^|[^A-Z])(' + COMMON_ABBREVIATIONS.map(regexQuote).join('|') + ')($|[^a-z])', 'g');
+
+function ucfirst(s: string) {
+    return s.substr(0, 1).toUpperCase() + s.substr(1).toLowerCase();
 }
