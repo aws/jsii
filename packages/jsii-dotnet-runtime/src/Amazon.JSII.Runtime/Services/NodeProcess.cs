0117533012735be57c02f597ebb9fac4c5d42d93
Hamza Assyad <hamzaad@amazon.com>
2019-08-20 14:45:22 -0700

    Miscellaneous .NET runtime changes - support for JSII_DEBUG and JSII_RUNTIME

--- packages/jsii-dotnet-runtime/src/Amazon.JSII.Runtime/Services/NodeProcess.cs
+++ packages/jsii-dotnet-runtime/src/Amazon.JSII.Runtime/Services/NodeProcess.cs
@@ -9,25 +9,36 @@ namespace Amazon.JSII.Runtime.Services
     {
         readonly Process _process;
         readonly ILogger _logger;
+        private const string JsiiRuntime = "JSII_RUNTIME";
+        private const string JsiiDebug = "JSII_DEBUG";
+        private const string JsiiAgent = "JSII_AGENT";
 
         public NodeProcess(IJsiiRuntimeProvider jsiiRuntimeProvider, ILoggerFactory loggerFactory)
         {
             loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
             _logger = loggerFactory.CreateLogger<NodeProcess>();
 
+            var runtimePath = Environment.GetEnvironmentVariable(JsiiRuntime);
+            if (string.IsNullOrWhiteSpace(runtimePath))
+                runtimePath = jsiiRuntimeProvider.JsiiRuntimePath;
+            
             _process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "node",
-                    Arguments = "--max-old-space-size=4096 " + jsiiRuntimeProvider.JsiiRuntimePath,
+                    Arguments = "--max-old-space-size=4096 " + runtimePath,
                     RedirectStandardInput = true,
                     RedirectStandardOutput = true,
                     RedirectStandardError = true
                 }
             };
 
-            _process.StartInfo.EnvironmentVariables.Add("JSII_AGENT", "DotNet/" + Environment.Version);
+            _process.StartInfo.EnvironmentVariables.Add(JsiiAgent, "DotNet/" + Environment.Version);
+            
+            var debugPath = Environment.GetEnvironmentVariable(JsiiDebug);
+            if (!string.IsNullOrWhiteSpace(debugPath))
+                _process.StartInfo.EnvironmentVariables.Add(JsiiDebug, debugPath);
 
             _logger.LogDebug("Starting jsii runtime...");
             _logger.LogDebug($"{_process.StartInfo.FileName} {_process.StartInfo.Arguments}");
