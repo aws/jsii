194e93d56bca96ebe2f546a66d2b0fc51c3039f0
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-08-23 14:27:25 +0200

    chore(tests): migrate jsii tests to jest
    In an effort to try and stop using `nodeunit` everywhere, migrating all
    existing tests in the `jsii` module to `jest`.

---
+++ packages/jsii/test/enums.test.ts
@@ -0,0 +1,39 @@
+import { sourceToAssemblyHelper } from '../lib';
+
+// ----------------------------------------------------------------------
+test('test parsing enum with two members and no values', async () => {
+  const assembly = await sourceToAssemblyHelper(`
+    export enum Foo {
+      BAR,
+      BAZ
+    }
+  `);
+
+  expect(assembly.types!['testpkg.Foo']).toEqual({
+    assembly: 'testpkg',
+    fqn: 'testpkg.Foo',
+    kind: 'enum',
+    members: [{ name: 'BAR' }, { name: 'BAZ' }],
+    locationInModule: { filename: 'index.ts', line: 2 },
+    name: 'Foo'
+  });
+});
+
+// ----------------------------------------------------------------------
+test('test parsing enum with two members and assigned values', async () => {
+  const assembly = await sourceToAssemblyHelper(`
+    export enum Foo {
+      BAR = 'Bar',
+      BAZ = 'Baz'
+    }
+  `);
+
+  expect(assembly.types!['testpkg.Foo']).toEqual({
+    assembly: 'testpkg',
+    fqn: 'testpkg.Foo',
+    kind: 'enum',
+    members: [{ name: 'BAR' }, { name: 'BAZ' }],
+    locationInModule: { filename: 'index.ts', line: 2 },
+    name: 'Foo'
+  });
+});
