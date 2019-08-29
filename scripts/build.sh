59d853f0ad167f7578637beb84983e9fe87c5cae
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-08-29 16:16:52 +0200

    disable concurrency instead

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,10 +9,7 @@ export PATH=./node_modules/.bin:$PATH
 
 echo "============================================================================================="
 echo "building..."
-lerna run build --stream --sort --scope=jsii-dotnet-jsonmodel --include-filtered-dependencies
-lerna run build --stream --sort --scope=jsii-dotnet-runtime --include-filtered-dependencies
-lerna run build --stream --sort --scope=jsii-dotnet-analyzers --include-filtered-dependencies
-lerna run build --stream --sort --ignore=jsii-dotnet-jsonmodel --ignore=jsii-dotnet-runtime --ignore=jsii-dotnet-analyzers
+lerna run build --stream --sort --concurrency=1
 
 echo "============================================================================================="
 echo "testing..."
