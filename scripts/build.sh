e6bc943d570ca3c4b989c8ec6c98ea5308345aee
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-08-29 15:59:05 +0200

    fix build order of dotnet packages

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,10 @@ export PATH=./node_modules/.bin:$PATH
 
 echo "============================================================================================="
 echo "building..."
-lerna run build --stream --sort
+lerna run build --stream --sort --scope=jsii-dotnet-jsonmodel
+lerna run build --stream --sort --scope=jsii-dotnet-runtime
+lerna run build --stream --sort --scope=jsii-dotnet-analyzers
+lerna run build --stream --sort --ignore=jsii-dotnet-jsonmodel --ignore=jsii-dotnet-runtime --ignore=jsii-dotnet-analyzers
 
 echo "============================================================================================="
 echo "testing..."
