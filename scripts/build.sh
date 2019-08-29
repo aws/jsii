cf9c8166688786c219d85153876c9b32e035eb87
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-08-29 16:11:03 +0200

    account for build dependencies

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,9 @@ export PATH=./node_modules/.bin:$PATH
 
 echo "============================================================================================="
 echo "building..."
-lerna run build --stream --sort --scope=jsii-dotnet-jsonmodel
-lerna run build --stream --sort --scope=jsii-dotnet-runtime
-lerna run build --stream --sort --scope=jsii-dotnet-analyzers
+lerna run build --stream --sort --scope=jsii-dotnet-jsonmodel --include-filtered-dependencies
+lerna run build --stream --sort --scope=jsii-dotnet-runtime --include-filtered-dependencies
+lerna run build --stream --sort --scope=jsii-dotnet-analyzers --include-filtered-dependencies
 lerna run build --stream --sort --ignore=jsii-dotnet-jsonmodel --ignore=jsii-dotnet-runtime --ignore=jsii-dotnet-analyzers
 
 echo "============================================================================================="
