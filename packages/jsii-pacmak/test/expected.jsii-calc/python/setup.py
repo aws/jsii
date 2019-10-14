7c562bc92f7c42c63e4bf1442e6e474ca71d74f8
Shiv Lakshminarayan <shivlaks@amazon.com>
2019-10-14 14:07:18 -0700

    v0.19.0 (#879)
    See CHANGELOG

--- packages/jsii-pacmak/test/expected.jsii-calc/python/setup.py
+++ packages/jsii-pacmak/test/expected.jsii-calc/python/setup.py
@@ -4,7 +4,7 @@ import setuptools
 kwargs = json.loads("""
 {
     "name": "jsii-calc",
-    "version": "0.18.0",
+    "version": "0.19.0",
     "description": "A simple calcuator built on JSII.",
     "url": "https://github.com/aws/jsii",
     "long_description_content_type": "text/markdown",
@@ -21,7 +21,7 @@ kwargs = json.loads("""
     ],
     "package_data": {
         "jsii_calc._jsii": [
-            "jsii-calc@0.18.0.jsii.tgz"
+            "jsii-calc@0.19.0.jsii.tgz"
         ],
         "jsii_calc": [
             "py.typed"
@@ -29,11 +29,11 @@ kwargs = json.loads("""
     },
     "python_requires": ">=3.6",
     "install_requires": [
-        "jsii~=0.18.0",
+        "jsii~=0.19.0",
         "publication>=0.0.3",
-        "scope.jsii-calc-base~=0.18.0",
-        "scope.jsii-calc-base-of-base~=0.18.0",
-        "scope.jsii-calc-lib~=0.18.0"
+        "scope.jsii-calc-base~=0.19.0",
+        "scope.jsii-calc-base-of-base~=0.19.0",
+        "scope.jsii-calc-lib~=0.19.0"
     ]
 }
 """)
---
+++ packages/jsii-sampiler/CHANGELOG.md
@@ -0,0 +1,16 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+# [0.19.0](https://github.com/aws/jsii/compare/v0.18.0...v0.19.0) (2019-10-14)
+
+
+### Bug Fixes
+
+* **sampiler:** Add missing .npmignore ([#875](https://github.com/aws/jsii/issues/875)) ([b16fc6b](https://github.com/aws/jsii/commit/b16fc6bdaf1825d53629c2a44b769f924ffb91d0))
+
+
+### Features
+
+* **sampiler:** translate code samples to Python ([#827](https://github.com/aws/jsii/issues/827)) ([c9a7002](https://github.com/aws/jsii/commit/c9a7002431c0db6224d595eb5555b916036d4575))
