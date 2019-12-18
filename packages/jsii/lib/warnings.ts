93cc2ffad12fa69df649902f05b68781aea5a822
Elad Ben-Israel <benisrae@amazon.com>
2019-12-18 14:12:32 +0200

    Update warnings.ts

--- packages/jsii/lib/warnings.ts
+++ packages/jsii/lib/warnings.ts
@@ -2,6 +2,6 @@
  * Indicates which warnings are currently enabled. By default all warnings are
  * enabled, and can be silenced through the --silence-warning option.
  */
-export const enabledWarnings: { [name]: boolean } = {
+export const enabledWarnings: { [name: string]: boolean } = {
   'reserved-word': true
 };
