05717cfb923ee16f8b58350023eda7cf9578957b
Rico Huijbers <huijbers@amazon.com>
2020-01-03 14:30:11 +0100

    release: v0.21.1
    See CHANGELOG

--- packages/jsii-pacmak/test/expected.jsii-calc-base-of-base/python/src/scope/jsii_calc_base_of_base/__init__.py
+++ packages/jsii-pacmak/test/expected.jsii-calc-base-of-base/python/src/scope/jsii_calc_base_of_base/__init__.py
@@ -8,7 +8,7 @@ import jsii
 import jsii.compat
 import publication
 
-__jsii_assembly__ = jsii.JSIIAssembly.load("@scope/jsii-calc-base-of-base", "0.21.0", __name__, "jsii-calc-base-of-base@0.21.0.jsii.tgz")
+__jsii_assembly__ = jsii.JSIIAssembly.load("@scope/jsii-calc-base-of-base", "0.21.1", __name__, "jsii-calc-base-of-base@0.21.1.jsii.tgz")
 
 
 @jsii.interface(jsii_type="@scope/jsii-calc-base-of-base.IVeryBaseInterface")
