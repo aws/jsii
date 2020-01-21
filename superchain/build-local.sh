b0a2bf7a3cc295911faf197f143c3c03b19d0ebe
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2020-01-21 10:20:26 +0100

    chore: annotate Docker image with jsii version
    This allows easily determining which jsii version a particular Docker
    image was built for / tested against, while we don't have a better
    scheme for versionning the image itself.

--- superchain/build-local.sh
+++ superchain/build-local.sh
@@ -24,5 +24,6 @@ docker build                                                                    
   --pull                                                                        \
   --build-arg BUILD_TIMESTAMP=$(date -u +'%Y-%m-%dT%H:%M:%SZ')                  \
   --build-arg COMMIT_ID=${COMMIT_ID}                                            \
+  --build-arg JSII_VERSION=$(node -p "require('../lerna.json').version")        \
   -t "jsii/superchain:local"                                                    \
   .
