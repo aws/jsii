c32443979548faa283894d5aaf4f4baf31350063
Romain Marcadier-Muller <rmuller@amazon.com>
2020-02-07 17:31:18 +0100

    feat(dotnet): upgrade target framework to netcoreapp3.1 (#1241)
    As `netcoreapp3.0` is going to be EOL pretty soon (early March 2020),
    upgrading our target framework to `netcoreapp3.1` early in order to
    catch any surprises. Since `netcoreapp3.1` is nothing but an LTS version
    of `netcoreapp3.0` (plus some bug fixes), it should be transparent to users
    who may only need to upgrade their .NET SDK.

--- packages/@jsii/dotnet-runtime-test/test/Amazon.JSII.Runtime.IntegrationTests/ComplianceTests.cs
+++ packages/@jsii/dotnet-runtime-test/test/Amazon.JSII.Runtime.IntegrationTests/ComplianceTests.cs
@@ -924,7 +924,7 @@ namespace Amazon.JSII.Runtime.IntegrationTests
         [Fact(DisplayName = Prefix + nameof(JsiiAgent))]
         public void JsiiAgent()
         {
-            Assert.Equal("DotNet/" + Environment.Version + "/.NETCoreApp,Version=v3.0/1.0.0.0", JsiiAgent_.JsiiAgent);
+            Assert.Equal("DotNet/" + Environment.Version + "/.NETCoreApp,Version=v3.1/1.0.0.0", JsiiAgent_.JsiiAgent);
         }
 
         [Fact(DisplayName = Prefix + nameof(ReceiveInstanceOfPrivateClass))]
