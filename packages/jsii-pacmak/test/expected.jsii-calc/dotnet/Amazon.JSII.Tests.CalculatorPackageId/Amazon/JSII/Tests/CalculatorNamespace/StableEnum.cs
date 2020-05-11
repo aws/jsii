8569c00045f8ee25356e8fa3074674558ec41732
Rico Huijbers <rix0rrr@gmail.com>
2020-05-11 13:04:30 +0200

    fix(pacmak): label "External" stability as "Stable" (#1633)
    The distinction between "Stable" (checked stability) and "External"
    (unchecked stability) is lost on users, and honestly we shouldn't bother
    them with it.
    
    Treat them the same when generating code.
    
    Co-authored-by: Romain Marcadier <rmuller@amazon.com>

--- packages/jsii-pacmak/test/expected.jsii-calc/dotnet/Amazon.JSII.Tests.CalculatorPackageId/Amazon/JSII/Tests/CalculatorNamespace/StableEnum.cs
+++ packages/jsii-pacmak/test/expected.jsii-calc/dotnet/Amazon.JSII.Tests.CalculatorPackageId/Amazon/JSII/Tests/CalculatorNamespace/StableEnum.cs
@@ -5,20 +5,11 @@ using Amazon.JSII.Runtime.Deputy;
 namespace Amazon.JSII.Tests.CalculatorNamespace
 {
 
-    /// <remarks>
-    /// <strong>Stability</strong>: Stable
-    /// </remarks>
     [JsiiEnum(nativeType: typeof(StableEnum), fullyQualifiedName: "jsii-calc.StableEnum")]
     public enum StableEnum
     {
-        /// <remarks>
-        /// <strong>Stability</strong>: Stable
-        /// </remarks>
         [JsiiEnumMember(name: "OPTION_A")]
         OPTION_A,
-        /// <remarks>
-        /// <strong>Stability</strong>: Stable
-        /// </remarks>
         [JsiiEnumMember(name: "OPTION_B")]
         OPTION_B
     }
