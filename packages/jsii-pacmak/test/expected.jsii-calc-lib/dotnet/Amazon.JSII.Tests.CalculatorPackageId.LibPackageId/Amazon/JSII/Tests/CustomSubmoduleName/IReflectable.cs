b0330a2cf37ad05c3b2869fec69f36935e543338
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2020-05-19 13:47:08 +0200

    feat(dotnet): allow customizing submodule namespaces
    Allows customizing the .Net namespace used by submodules by adding new
    `targets.dotnet.namespace` entries in the `.jsiirc.json` file.

--- packages/jsii-pacmak/test/expected.jsii-calc-lib/dotnet/Amazon.JSII.Tests.CalculatorPackageId.LibPackageId/Amazon/JSII/Tests/CustomSubmoduleName/IReflectable.cs
+++ packages/jsii-pacmak/test/expected.jsii-calc-lib/dotnet/Amazon.JSII.Tests.CalculatorPackageId.LibPackageId/Amazon/JSII/Tests/CustomSubmoduleName/IReflectable.cs
@@ -2,7 +2,7 @@ using Amazon.JSII.Runtime.Deputy;
 
 #pragma warning disable CS0672,CS0809,CS1591
 
-namespace Amazon.JSII.Tests.CalculatorNamespace.LibNamespace.Submodule
+namespace Amazon.JSII.Tests.CustomSubmoduleName
 {
     /// <remarks>
     /// <strong>Stability</strong>: Deprecated
@@ -16,7 +16,7 @@ namespace Amazon.JSII.Tests.CalculatorNamespace.LibNamespace.Submodule
         /// </remarks>
         [JsiiProperty(name: "entries", typeJson: "{\"collection\":{\"elementtype\":{\"fqn\":\"@scope/jsii-calc-lib.submodule.ReflectableEntry\"},\"kind\":\"array\"}}")]
         [System.Obsolete()]
-        Amazon.JSII.Tests.CalculatorNamespace.LibNamespace.Submodule.IReflectableEntry[] Entries
+        Amazon.JSII.Tests.CustomSubmoduleName.IReflectableEntry[] Entries
         {
             get;
         }
