2c3e733e4276069ead5159c2e9078b761da2b8b0
Hsing-Hui Hsu <hsinghui@amazon.com>
2020-08-13 17:26:08 -0700

    wip generate interfaces for classes, uppercase props

--- packages/jsii-pacmak/lib/targets/golang/types/class.ts
+++ packages/jsii-pacmak/lib/targets/golang/types/class.ts
@@ -3,13 +3,15 @@ import { CodeMaker } from 'codemaker';
 import { TypeMapper } from './interface';
 import { GoType } from './go-type';
 
+const CLASS_INTERFACE_SUFFIX = 'Iface';
+
 export class GoClass extends GoType {
   public constructor(public type: ClassType) {
     super(type);
   }
 
   public emit(code: CodeMaker): void {
-    // emitClassInterface(code);
+    this.emitClassInterface(code);
 
     code.openBlock(`type ${this.localName} struct`);
 
@@ -25,16 +27,20 @@ export class GoClass extends GoType {
     );
   }
 
+  // TODO deal with immutability?
+  //
   private emitClassProperty(code: CodeMaker, property: Property) {
     const type = new TypeMapper(property.type).mapType();
+    const propName = code.toPascalCase(property.name);
 
-    code.line(`${property.name} ${type}`);
+    code.line(`${propName} ${type}`);
   }
 
   private emitClassMethod(code: CodeMaker, method: Method) {
     const returnType = method.returns.type.void
       ? ''
-      : ` ${new TypeMapper(method.returns.type).mapType()}`;
+      : ` ${new TypeMapper(method.returns.type).mapType()}`; // TODO typemapper needs to handle cthings like Array etc
+
     const instanceArg = this.localName.substring(0, 1).toLowerCase();
     const methodName = code.toPascalCase(method.name);
 
@@ -48,6 +54,10 @@ export class GoClass extends GoType {
   }
 
   // Generate interface that defines getters for public properties and any method signatures
-  // private emitClassInterface(code: CodeMaker) {
-  // }
+  private emitClassInterface(code: CodeMaker) {
+    const interfaceName = this.localName + CLASS_INTERFACE_SUFFIX;
+    code.openBlock(`type ${interfaceName} interface`);
+    code.closeBlock();
+    code.line();
+  }
 }
