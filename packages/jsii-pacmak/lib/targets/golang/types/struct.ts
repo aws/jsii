4997976c9313d955097beda19261b465c53ecc5c
Hsing-Hui Hsu <hsinghui@amazon.com>
2020-08-18 08:56:59 -0700

    chore: include generation of struct methods

--- packages/jsii-pacmak/lib/targets/golang/types/struct.ts
+++ packages/jsii-pacmak/lib/targets/golang/types/struct.ts
@@ -1,7 +1,7 @@
 import { GoType } from './go-type';
 import { TypeMapper } from './interface'; // TODO replace with GoTypeRef
 import { InterfaceType, Property } from 'jsii-reflect';
-import { CodeMaker } from 'codemaker';
+import { CodeMaker, toPascalCase } from 'codemaker';
 
 // JSII datatype interfaces, aka structs
 export class Struct extends GoType {
@@ -12,14 +12,15 @@ export class Struct extends GoType {
 
     // TODO check if datatype? (isDataType() on jsii-reflect seems wrong)
     this.properties = Object.values(this.type.getProperties()).map(
-      (prop) => new StructProperty(prop),
+      (prop) => new StructProperty(this, prop),
     );
   }
 
-  // needs to generate both a Go interface and a struct
+  // needs to generate both a Go interface and a struct, as well as the methods on the struct
   public emit(code: CodeMaker): void {
     this.generateInterface(code);
     this.generateStruct(code);
+    this.generateImpl(code);
   }
 
   private generateInterface(code: CodeMaker): void {
@@ -39,26 +40,49 @@ export class Struct extends GoType {
     code.closeBlock();
     code.line();
   }
+
+  private generateImpl(code: CodeMaker): void {
+    code.line();
+    this.properties.forEach((property) => property.emitMethod(code));
+    code.line();
+  }
 }
 
 // StructProperty encapsulates logic for public properties on the concrete struct
 export class StructProperty {
   public readonly name: string;
+  public readonly getter: string;
   public readonly returnType: string;
 
-  public constructor(public readonly property: Property) {
-    this.name = this.property.name;
+  public constructor(
+    public readonly parent: Struct,
+    public readonly property: Property,
+  ) {
+    this.name = toPascalCase(this.property.name);
+    this.getter = `Get${this.name}`;
     const returnType = new TypeMapper(property.type).mapType(); // TODO replace with GoTypeRef
     this.returnType = returnType;
   }
 
   public emitProperty(code: CodeMaker) {
-    const propName = code.toPascalCase(this.name);
-    code.line(`${propName} ${this.returnType}`); // TODO figure out gofmt for indentation?
+    code.line(`${this.name} ${this.returnType}`); // TODO figure out gofmt for indentation?
   }
 
   public emitGetter(code: CodeMaker) {
-    const propName = code.toPascalCase(this.name);
-    code.line(`Get${propName}() ${this.returnType}`);
+    code.line(`${this.getter}() ${this.returnType}`);
+  }
+
+  public emitMethod(code: CodeMaker) {
+    const receiver = this.parent.localName;
+    const instanceArg = receiver.substring(0, 1).toLowerCase();
+
+    code.openBlock(
+      `func (${instanceArg} ${receiver}) ${
+        this.getter
+      }()${` ${this.returnType}`}`,
+    );
+    code.line(`return ${instanceArg}.${this.name}`);
+    code.closeBlock();
+    code.line();
   }
 }
