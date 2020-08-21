0021cf109767d4042a91e5113b5ae664eb6af842
Hsing-Hui Hsu <hsinghui@amazon.com>
2020-08-20 18:01:03 -0700

    chore: extract GoStruct
    Use this as base class for generation logic for datatype interfaces and classes

--- packages/jsii-pacmak/lib/targets/golang/types/class.ts
+++ packages/jsii-pacmak/lib/targets/golang/types/class.ts
@@ -1,52 +1,38 @@
 import { Method, ClassType } from 'jsii-reflect';
 import { CodeMaker } from 'codemaker';
 import { GoTypeRef } from './go-type-reference';
-import { GoProperty, GoType, GoEmitter } from './go-type';
-// import { Struct } from './struct';
+import { GoStruct } from './go-type';
 import { TypeField } from './type-field';
 import { Package } from '../package';
-import { getFieldDependencies } from '../util';
 
-const CLASS_INTERFACE_SUFFIX = 'Iface';
 /*
  * GoClass wraps a Typescript class as a Go custom struct type
  */
-export class GoClass extends GoType implements GoEmitter {
-  public readonly properties: GoProperty[];
-  public readonly interfaceName: string;
+export class GoClass extends GoStruct {
   public readonly methods: ClassMethod[];
 
-  public constructor(parent: Package, public type: ClassType) {
+  public constructor(parent: Package, type: ClassType) {
     super(parent, type);
-
-    this.properties = Object.values(this.type.getProperties()).map(
-      (prop) => new GoProperty(this, prop),
-    );
 
     this.methods = Object.values(this.type.getMethods()).map(
       (method) => new ClassMethod(this, method),
     );
-
-    this.interfaceName = `${this.name}${CLASS_INTERFACE_SUFFIX}`;
   }
 
   public emit(code: CodeMaker): void {
-    this.emitInterface(code);
-    this.emitStruct(code);
-    this.generateImpl(code);
+    super.emit(code);
 
     for (const method of this.methods) {
       method.emit(code);
     }
   }
 
-  // Generate interface that defines getters for public properties and any method signatures
-  private emitInterface(code: CodeMaker) {
+  protected emitInterface(code: CodeMaker): void {
     code.openBlock(`type ${this.interfaceName} interface`);
 
     for (const property of this.properties) {
       property.emitGetter(code);
-      property.emitSetter(code); // TODO might not need
+      property.emitSetter(code);
     }
 
     for (const method of this.methods) {
@@ -55,36 +41,6 @@ export class GoClass extends GoType implements GoEmitter {
 
     code.closeBlock();
     code.line();
-  }
-
-  private emitStruct(code: CodeMaker): void {
-    code.openBlock(`type ${this.name} struct`);
-
-    for (const property of this.properties) {
-      property.emitProperty(code);
-    }
-
-    code.closeBlock();
-    code.line();
-  }
-
-  private generateImpl(code: CodeMaker): void {
-    if (this.properties.length !== 0) {
-      code.line();
-
-      for (const property of this.properties) {
-        property.emitMethod(code);
-      }
-
-      code.line();
-    }
-  }
-
-  public get dependencies(): Package[] {
-    return [
-      ...getFieldDependencies(this.properties),
-      ...getFieldDependencies(this.methods),
-    ];
   }
 }
 
