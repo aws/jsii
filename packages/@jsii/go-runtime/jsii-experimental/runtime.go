85764deffd70af92333363bf38478440351654da
Romain Marcadier <rmuller@amazon.com>
2020-10-02 23:53:17 +0200

    feat(go): embed jsii runtime application (#2066)
    Bundles the `@jsii/runtime` application within the go runtime library.
    This ships the necessary files as byte slices in
    `embeddedruntime.generated.go`. The `client` initializer will un-pack
    this into a temporary directory (cleaned up by `client.Close()`) and run
    that, unless the user specified the `JSII_RUNTIME` environment variable
    (in which case, that command is used instead).
    
    The `client` also assigns `JSII_AGENT` to a string representing the `go`
    stack version used during build (`runtime.Version()`), the operating
    system under which the application is running and the CPU architecture.
    The string is formatted as `VERSION/OS/ARCH`.
    
    A new `runtime.go` was introduced that manages a singleton `client`
    instance and exports a `Close` function that can be invoked to
    gracefully clean up the child process.
    
    
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

---
+++ packages/@jsii/go-runtime/jsii-experimental/runtime.go
@@ -0,0 +1,12 @@
+package jsii
+
+// Close finalizes the runtime process, signalling the end of the execution to
+// the jsii kernel process, and waiting for graceful termination. The best
+// practice is to defer call thins at the beginning of the "main" function.
+//
+// If a jsii client is used *after* Close was called, a new jsii kernel process
+// will be initialized, and Close should be called again to correctly finalize
+// that, too. This behavior is intended for use in unit/integration tests.
+func Close() {
+	closeClient()
+}
