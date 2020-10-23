dcaf0980c4ba6a967c8e5ab6a8075fcbed677637
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2020-10-23 14:25:49 +0200

    remove old expecttation files

--- packages/jsii-pacmak/test/expected.jsii-calc/kotlin/src/main/kotlin/software/amazon/jsii/tests/kotlin/calculator/submodule/child/SomeEnum.kt
+++
@@ -1,8 +0,0 @@
-package software.amazon.jsii.tests.kotlin.calculator.submodule.child
-
-@javax.annotation.Generated("jsii-pacmak")
-@software.amazon.jsii.Stability(software.amazon.jsii.Stability.Level.Experimental)
-enum class SomeEnum {
-    @software.amazon.jsii.Stability(software.amazon.jsii.Stability.Level.Experimental)
-    SOME,
-}
