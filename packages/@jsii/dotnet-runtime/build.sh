dc3ac540c8465966bd966da416c6e5f52686151d
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2020-12-21 11:49:25 +0100

    fix dotnet runtime bundling order

--- packages/@jsii/dotnet-runtime/build.sh
+++ packages/@jsii/dotnet-runtime/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# embed @jsii/runtime as a resource
-bundle_dir="src/Amazon.JSII.Runtime/jsii-runtime"
-mkdir -p ${bundle_dir}
-rsync -av $(node -p 'require("path").dirname(require.resolve("@jsii/runtime/package.json"))')/webpack/ ${bundle_dir}
-
 # Build just Runtime and it's dependencies instead of the
 # solution to avoid integration tests from trying to be
 # built before the calc packages are generated.
--- packages/@jsii/dotnet-runtime/generate.sh
+++ packages/@jsii/dotnet-runtime/generate.sh
@@ -6,5 +6,11 @@ src="./src"
 /usr/bin/env node ./Directory.Build.props.t.js > ${src}/Directory.Build.props
 /usr/bin/env node ./NuGet.Metadata.props.t.js > ${src}/NuGet.Metadata.props
 
+# Embed @jsii/runtime as a resource
+bundle_dir="src/Amazon.JSII.Runtime/jsii-runtime"
+mkdir -p ${bundle_dir}
+rsync -av $(node -p 'require("path").dirname(require.resolve("@jsii/runtime/package.json"))')/webpack/ ${bundle_dir}
+
+
 # Generate Source
 /usr/bin/env node ./JsiiRuntimeProvider.t.js > ${src}/Amazon.JSII.Runtime/Services/JsiiRuntimeProvider.cs
