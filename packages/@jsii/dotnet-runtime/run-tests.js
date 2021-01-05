eb4e9ce9c142461bd06685da450e675f243c9494
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-01-05 18:10:44 +0100

    actually check in test runner

---
+++ packages/@jsii/dotnet-runtime/run-tests.js
@@ -0,0 +1,42 @@
+const cp = require('child_process');
+const os = require('os');
+const path = require('path');
+const process = require('process');
+
+const FLAG = '--experimental-worker';
+
+let nodeOptions = process.allowedNodeEnvironmentFlags.has(FLAG)
+  ? `${process.env.NODE_OPTIONS ?? ''} ${FLAG}`.trim()
+  : process.env.NODE_OPTIONS;
+
+const result = cp.spawnSync(
+  'dotnet',
+  [
+    'test',
+    '--no-restore',
+    '-c',
+    'Release',
+    path.join(__dirname, 'src', 'Amazon.JSII.Runtime.sln'),
+  ],
+  {
+    env: {
+      ...process.env,
+      NODE_OPTIONS: nodeOptions,
+    },
+    shell: false,
+    stdio: 'inherit',
+    windowsHide: true,
+  },
+);
+
+console.error(`dotnet CLI returned: ${JSON.stringify(result, null, 2)}`);
+
+if (result.error) {
+  throw result.error;
+}
+
+if (result.signal) {
+  process.exit(-os.constants.signals[result.signal]);
+}
+
+process.exit(result.status);
