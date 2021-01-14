9ffd20481bd804ff288ab4bf91b9f394d52d3d90
Mitchell Valine <valinm@amazon.com>
2021-01-14 09:57:53 -0800

    feat(go): runtime release tagging (#2417)
    Adds github actions workflows for automatically tagging jsii-runtime-go
    when jsii release branch is updated. This essentially publishes new
    runtime version. This may eventually be served better in delivlib but is
    much simpler in github actions for the alpha phase.
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Romain Marcadier <rmuller@amazon.com>

--- packages/@jsii/go-runtime/build-tools/build-repo-dir.ts
+++ packages/@jsii/go-runtime/build-tools/build-repo-dir.ts
@@ -1,21 +1,10 @@
 #!/usr/bin/env npx ts-node
 
-import { SpawnOptions } from 'child_process';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-import { runCommand } from './_constants';
-
 // Constants
 const RUNTIME_DIR = path.resolve(__dirname, '..', 'jsii-experimental');
-// Destination repo configuration
-const REPO_NAME = process.env.JSII_RUNTIME_REPO_NAME;
-const USERNAME = process.env.JSII_RUNTIME_REPO_USERNAME;
-const TOKEN = process.env.JSII_RUNTIME_REPO_TOKEN;
-const REMOTE =
-  USERNAME && TOKEN
-    ? `https://${USERNAME}:${TOKEN}@github.com/${REPO_NAME}.git`
-    : `https://github.com${REPO_NAME}.git`;
 
 // Always preserve these files/paths from the cloned repo
 const PRESERVE = ['.git', '.gitignore'];
@@ -28,18 +17,10 @@ const DONT_OVERWRITE = [...PRESERVE, ...ROOT_FILES, ...LOCAL_FILES];
 
 async function main() {
   const args = process.argv.slice(2);
-  console.log(args[0]);
   await makeRepoDir(args[0]);
 }
 
 async function makeRepoDir(dir: string) {
-  const runRepoCmd = (
-    cmd: string,
-    args: readonly string[],
-    opts: SpawnOptions = {},
-  ) => runCommand(cmd, args, { cwd: dir, ...opts });
-  runRepoCmd('git', ['clone', REMOTE, '.'], { stdio: 'inherit' });
-
   // Clone repo and delete its contents except nodes in the PRESERVE list.
   const clonedFiles = await fs.readdir(dir);
   await Promise.all(
