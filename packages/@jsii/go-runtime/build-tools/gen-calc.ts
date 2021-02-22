0b59a6616a385bf1f0b53e84575c785d1844c8ef
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-02-22 15:05:56 +0100

    feat(go): emit indirect dependencies in go.mod
    This helps IDEs (eg: GoLand from JetBrains, etc..) be more comfortable
    with the codebase, as indirect dependencies are correctly mapped. This
    generates code more in line with what `go mod` would have generated if
    one had tried to manually generate those packages.

--- packages/@jsii/go-runtime/build-tools/gen-calc.ts
+++ packages/@jsii/go-runtime/build-tools/gen-calc.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env npx ts-node
 
-import { removeSync } from 'fs-extra';
-import { join, resolve } from 'path';
+import { readFileSync, removeSync, writeFileSync } from 'fs-extra';
+import { join, relative, resolve } from 'path';
 
+import { runtimeModules } from '../lib';
+import { localRuntimeModules } from '../lib/local-runtime-modules';
 import { runCommand } from './_constants';
 
 const genRoot = join(__dirname, '..', 'jsii-calc');
@@ -22,3 +24,45 @@ runCommand(
   ],
   { stdio: 'inherit' },
 );
+
+// Inject "replaces" in the go.mod files so IDEs do not struggle too much...
+const genModules = localRuntimeModules(genRoot);
+const localModules = {
+  ...genModules,
+  ...runtimeModules,
+};
+for (const localPath of Object.values(genModules)) {
+  const goModFile = join(localPath, 'go.mod');
+  const goMod = readFileSync(goModFile, 'utf8');
+
+  const replaces = new Array<{
+    readonly dep: string;
+    readonly depPath: string;
+  }>();
+
+  let matches: ReturnType<RegExp['exec']>;
+  const depRegex = /([a-z0-9._~/-]+)\s+v\d/gi;
+  while ((matches = depRegex.exec(goMod)) != null) {
+    const [, dep] = matches;
+    if (dep in localModules) {
+      const depPath = localModules[dep];
+      replaces.push({ dep, depPath });
+    }
+  }
+
+  writeFileSync(
+    goModFile,
+    [
+      goMod.trim(),
+      '',
+      'replace (',
+      ...replaces
+        .map(
+          ({ dep, depPath }) => `\t${dep} => ${relative(localPath, depPath)}`,
+        )
+        .sort(),
+      ')',
+      '',
+    ].join('\n'),
+  );
+}
