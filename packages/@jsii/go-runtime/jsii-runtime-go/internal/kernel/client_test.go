331bea10add85979848afe90c09e27c08480e153
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-03-05 10:46:30 +0100

    add test that verifies type registry survives CloseClient call

--- packages/@jsii/go-runtime/jsii-runtime-go/internal/kernel/client_test.go
+++ packages/@jsii/go-runtime/jsii-runtime-go/internal/kernel/client_test.go
@@ -1,15 +1,15 @@
 package kernel
 
 import (
-	"fmt"
+	"github.com/aws/jsii-runtime-go/internal/typeregistry"
+	"reflect"
 	"testing"
 )
 
 func TestClient(t *testing.T) {
 	client, err := newClient()
 	if err != nil {
-		t.Log(err)
-		panic(fmt.Sprintf("Client init error: %s", err.Error()))
+		t.Fatalf("client init failed: %s", err.Error())
 	}
 	defer client.close()
 
@@ -24,6 +24,38 @@ func TestClient(t *testing.T) {
 		t.Log(res)
 		if err != nil {
 			t.Log(err)
+		}
+	})
+
+	t.Run("Type registry survives CloseClient()", func(t *testing.T) {
+		client, err := newClient()
+		if err != nil {
+			t.Fatalf("client init failed: %s", err.Error())
+		}
+
+		// Clean up after ourselves, so this test does not leave traces behind.
+		defer func() { types = typeregistry.New() }()
+
+		type enumType string
+		var enumTypeFOO enumType = "FOO"
+
+		registry := client.Types()
+		err = registry.RegisterEnum(
+			"example.Enum",
+			reflect.TypeOf((*enumType)(nil)).Elem(),
+			map[string]interface{}{"FOO": enumTypeFOO},
+		)
+		if err != nil {
+			t.Fatalf("failed registering enum: %s", err.Error())
+		}
+
+		CloseClient()
+
+		// Getting the registry from the client again.
+		registry = client.Types()
+
+		if _, ok := registry.TryRenderEnumRef(reflect.ValueOf(enumTypeFOO)); !ok {
+			t.Errorf("failed rendering enum ref, it should have worked!")
 		}
 	})
 }
