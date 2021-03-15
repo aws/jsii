99b3b71f727c9c8fe29a0c1000bbfb1fffbf422c
Elad Ben-Israel <benisrae@amazon.com>
2021-03-15 08:52:28 +0200

    fix(go): duplicate conversion functions when parent structs have the same base name
    If a struct has two parent structs with the same base name (but different packages), the emitted conversion function will have the same name (`ToParentStruct` and `ToParentStruct`).
    
    Since we are not even sure that these base conversion functions are required, omit them for now and we can decide to restore them at a later stage if the use case is clearer.
    
    Fixes #2692

--- packages/jsii-calc/lib/index.ts
+++ packages/jsii-calc/lib/index.ts
@@ -13,3 +13,4 @@ export * as onlystatic from './only-static';
 export * as nodirect from './no-direct-types';
 export * as module2647 from './module2647';
 export * as module2689 from './module2689';
+export * as module2692 from './module2692';
