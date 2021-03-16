2dbe84dfeff8d6f63aab19c1674fb7c9d17ea976
Elad Ben-Israel <benisrae@amazon.com>
2021-03-16 08:18:49 +0200

    fix(go): invalid code when a module only has static methods [test only] (#2704)
    This was already fixed by #2622. This PR introduces a test to avoid regression.
    
    Fixes #2617

--- packages/jsii-calc/lib/index.ts
+++ packages/jsii-calc/lib/index.ts
@@ -12,6 +12,7 @@ export * as submodule from './submodule';
 export * as onlystatic from './only-static';
 export * as nodirect from './no-direct-types';
 export * as module2647 from './module2647';
+export * as module2617 from './module2617';
 export * as module2689 from './module2689';
 export * as module2692 from './module2692';
 export * as module2530 from './module2530';
