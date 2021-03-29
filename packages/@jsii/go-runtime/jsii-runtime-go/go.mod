0f0f5240ebf8149301c083d7a66b3c906d3e7c37
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-03-29 15:53:33 +0200

    feat(go): variadic type helpers to build slices
    It is somewhat inconvenient to build slices of optional values, and
    new variadic helpers can help soothe that pain in many circumstances...
    
    Adding variadic slice helpers:
    - `Bools`
    - `Numbers`
    - `Strings`
    - `Times`

--- packages/@jsii/go-runtime/jsii-runtime-go/go.mod
+++ packages/@jsii/go-runtime/jsii-runtime-go/go.mod
@@ -2,4 +2,7 @@ module github.com/aws/jsii-runtime-go
 
 go 1.16
 
-require github.com/Masterminds/semver/v3 v3.1.1
+require (
+	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/stretchr/testify v1.7.0
+)
---
+++ packages/@jsii/go-runtime/jsii-runtime-go/runtime_test.go
@@ -0,0 +1,43 @@
+package jsii
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestBool(t *testing.T) {
+	assert.Equal(t, true, *Bool(true))
+	assert.Equal(t, false, *Bool(false))
+}
+
+func TestBools(t *testing.T) {
+	assert.Equal(t, []*bool{Bool(true), Bool(false), Bool(false), Bool(true)}, *Bools(true, false, false, true))
+}
+
+func TestNumber(t *testing.T) {
+	assert.Equal(t, 123.45, *Number(123.45))
+	assert.Equal(t, 1337.0, *Number(1337))
+}
+
+func TestNumbers(t *testing.T) {
+	assert.Equal(t, []*float64{Number(42), Number(1337)}, *Numbers(42, 1337))
+}
+
+func TestString(t *testing.T) {
+	assert.Equal(t, "Hello", *String("Hello"))
+}
+
+func TestStrings(t *testing.T) {
+	assert.Equal(t, []*string{String("Hello"), String("World")}, *Strings("Hello", "World"))
+}
+
+func TestTime(t *testing.T) {
+	now := time.Now()
+	assert.Equal(t, now, *Time(now))
+}
+
+func TestTimes(t *testing.T) {
+	now := time.Now()
+	assert.Equal(t, []*time.Time{Time(now)}, *Times(now))
+}
