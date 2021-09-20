b261e9ae8c21a1ddfce4eb0a893bdab441b04b3a
Otavio Macedo <otaviom@amazon.com>
2021-09-20 13:14:12 +0100

    Detection of deprecated properties

--- packages/jsii/lib/transforms/deprecated-warnings-transformer.ts
+++ packages/jsii/lib/transforms/deprecated-warnings-transformer.ts
@@ -11,9 +11,24 @@ import { Block } from 'typescript';
 import * as bindings from '../node-bindings';
 import { fullyQualifiedName, isDeprecated } from './utils';
 
+type MethodLikeDeclaration =
+  | ts.MethodDeclaration
+  | ts.ConstructorDeclaration
+  | ts.FunctionDeclaration
+  | ts.GetAccessorDeclaration
+  | ts.SetAccessorDeclaration;
+
+enum ElementType {
+  METHOD = 0,
+  FUNCTION = 1,
+  CLASS = 2,
+  PARAMETER = 3,
+}
+
 interface Warning {
   readonly elementName: string;
   readonly message: string;
+  readonly type: ElementType;
 }
 
 export class DeprecatedWarningInjector {
@@ -63,7 +78,8 @@ export class DeprecatedWarningInjector {
 
 class DeprecatedWarningsTransformer {
   private addWarning = false;
-  private warning?: Warning;
+  private warnings: Warning[] = [];
+  private readonly typesWithDeprecatedProperties = new Map<ts.Type, ts.Node>();
 
   public constructor(
     private readonly typeChecker: ts.TypeChecker,
@@ -76,6 +92,10 @@ class DeprecatedWarningsTransformer {
   ) {}
 
   public transform<T extends ts.Node>(node: T): T {
+    if (this.index.size === 0) {
+      // Processing didn't happen, probably due to JSII compilation errors
+      return node;
+    }
     return this.visitEachChild(node);
   }
 
@@ -84,8 +104,16 @@ class DeprecatedWarningsTransformer {
   }
 
   private visitor<T extends ts.Node>(node: T): ts.VisitResult<T> {
+    if (ts.isPropertyDeclaration(node) || ts.isPropertySignature(node)) {
+      if (node.parent != null && isDeprecated(node)) {
+        const type = this.typeChecker.getTypeAtLocation(node.parent);
+        this.typesWithDeprecatedProperties.set(type, node);
+      }
+    }
+
     if (ts.isClassDeclaration(node)) {
-      const deprecatedType = this.findDeprecated([
+      this.warnings = [];
+      const deprecatedType = this.findDeprecatedInTheTypeHierarchy([
         this.typeChecker.getTypeAtLocation(node),
       ]);
       this.addWarning = deprecatedType != null;
@@ -93,109 +121,85 @@ class DeprecatedWarningsTransformer {
         const deprecatedNode = this.index.get(
           this.typeChecker.getFullyQualifiedName(deprecatedType!.symbol),
         );
-        this.warning = getWarning(this.typeChecker, deprecatedNode!);
+        this.warnings.push(
+          getWarning(this.typeChecker, deprecatedNode!, ElementType.CLASS),
+        );
       }
     }
 
     if (ts.isFunctionDeclaration(node)) {
-      this.addWarning = isDeprecated(node);
-      if (this.addWarning) {
-        this.warning = getWarning(this.typeChecker, node);
+      this.warnings = this.getParameterWarnings(node);
+      if (isDeprecated(node)) {
+        this.warnings.push(
+          getWarning(this.typeChecker, node, ElementType.FUNCTION),
+        );
       }
+      this.addWarning = this.warnings.length > 0;
     }
 
     if (isMethodLikeDeclaration(node)) {
-      this.addWarning = this.addWarning || isDeprecated(node);
+      const parameterWarnings = this.getParameterWarnings(
+        node as any as MethodLikeDeclaration,
+      );
+
+      parameterWarnings.forEach((warning: Warning) =>
+        this.warnings.push(warning),
+      );
+
       if (isDeprecated(node)) {
-        this.warning = getWarning(this.typeChecker, node);
+        this.warnings.push(
+          getWarning(this.typeChecker, node, ElementType.METHOD),
+        );
       }
+
+      this.addWarning = this.warnings.length > 0;
     }
 
     if (ts.isBlock(node) && this.addWarning) {
       const block = node as Block;
-      const warningStatement = this.createWarningStatement();
-      return ts.updateBlock(
-        node,
-        ts.createNodeArray([...warningStatement, ...block.statements]),
-      ) as any;
+      const nodeArray: ts.NodeArray<ts.Statement> = ts.createNodeArray([
+        ...this.createWarningStatements(),
+        ...block.statements,
+      ]);
+
+      // "Unstack" all warnings specific to this block
+      this.warnings = this.warnings.filter(
+        (warning) =>
+          ![ElementType.METHOD, ElementType.PARAMETER].includes(warning.type),
+      );
+      return ts.updateBlock(node, nodeArray) as any;
     }
 
     return this.visitEachChild(node);
   }
 
-  private createWarningStatement() {
-    const warning = this.warning;
-    const message = `[WARNING] ${warning?.elementName} is deprecated.\n  ${warning?.message}\n  This API will be removed in the net major release.`;
-
-    // TODO There should be a simpler way...
-    return [
-      // TODO Instead of declaring the function everywhere it's used, should we create a single declaration somewhere and reuse it?
-      //  If so, where should this declaration go?
-      ts.createFunctionDeclaration(
-        undefined,
-        undefined,
-        undefined,
-        'printWarning',
-        [],
-        [],
-        undefined,
-        ts.createBlock(
-          [
-            ts.createExpressionStatement(
-              ts.createAssignment(
-                ts.createIdentifier('const deprecated'),
-                ts.createIdentifier('process.env.DEPRECATED'),
-              ),
-            ),
-            ts.createExpressionStatement(
-              ts.createAssignment(
-                ts.createIdentifier('const deprecationMode'),
-                ts.createIdentifier(
-                  "['warn', 'error', 'quiet'].includes(deprecated) ? deprecated : \"warn\"",
-                ),
-              ),
-            ),
-            ts.createExpressionStatement(
-              ts.createAssignment(
-                ts.createIdentifier('const message'),
-                ts.createLiteral(
-                  `${warning?.elementName} is deprecated.\n  ${warning?.message}\n  This API will be removed in the net major release.`,
-                ),
-              ),
-            ),
-            // ts.createExpressionStatement(
-            ts.createSwitch(
-              ts.createIdentifier('deprecationMode'),
-              ts.createCaseBlock([
-                ts.createCaseClause(ts.createLiteral('error'), [
-                  ts.createThrow(
-                    ts.createNew(ts.createIdentifier('Error'), [], []),
-                  ),
-                  ts.createBreak(),
-                ]),
-                ts.createCaseClause(ts.createLiteral('warn'), [
-                  ts.createExpressionStatement(
-                    ts.createCall(
-                      ts.createIdentifier('console.warn'),
-                      [],
-                      [ts.createLiteral(message)],
-                    ),
-                  ),
-                  ts.createBreak(),
-                ]),
-              ]),
-            ),
-          ],
-          true,
+  private getParameterWarnings(node: MethodLikeDeclaration): Warning[] {
+    return node.parameters
+      .filter((parameter) =>
+        this.typesWithDeprecatedProperties.has(
+          this.typeChecker.getTypeAtLocation(parameter),
         ),
-      ),
-      ts.createExpressionStatement(
-        ts.createCall(ts.createIdentifier('printWarning'), [], []),
-      ),
-    ];
+      )
+      .map(
+        (parameter) =>
+          getWarning(
+            this.typeChecker,
+            this.typesWithDeprecatedProperties.get(
+              this.typeChecker.getTypeAtLocation(parameter),
+            )!,
+            ElementType.PARAMETER,
+          )!,
+      );
   }
 
-  private findDeprecated(toProcess: ts.Type[]): ts.Type | undefined {
+  private createWarningStatements(): ts.Statement[] {
+    return [...this.warnings].flatMap(createWarningStatement);
+  }
+
+  // TODO Rename this method
+  private findDeprecatedInTheTypeHierarchy(
+    toProcess: ts.Type[],
+  ): ts.Type | undefined {
     if (toProcess.length === 0) {
       return undefined;
     }
@@ -207,6 +211,12 @@ class DeprecatedWarningsTransformer {
     const node = this.index.get(
       this.typeChecker.getFullyQualifiedName(toProcess[0].symbol),
     )!;
+
+    // This type was not declared in the JSII assembly. Skipping.
+    if (node == null) {
+      return undefined;
+    }
+
     if (!ts.isClassDeclaration(node) && !ts.isInterfaceDeclaration(node)) {
       throw new Error('Node should be a class or interface declaration.');
     }
@@ -222,8 +232,86 @@ class DeprecatedWarningsTransformer {
       }
     }
 
-    return this.findDeprecated(newBatch);
+    return this.findDeprecatedInTheTypeHierarchy(newBatch);
   }
+}
+
+function createWarningStatement(warning: Warning): ts.Statement[] {
+  const message = `${warning?.elementName} is deprecated.\n  ${warning?.message}\n  This API will be removed in the net major release.`;
+  // TODO Are random numbers a good idea?
+  const functionName = `printWarning${getRandomInt(
+    0,
+    Number.MAX_SAFE_INTEGER,
+  )}`;
+
+  // TODO There must be a simpler way...
+  return [
+    // TODO Instead of declaring the function everywhere it's used, should we create a single declaration somewhere and reuse it?
+    //  If so, where should this declaration go?
+    ts.createFunctionDeclaration(
+      undefined,
+      undefined,
+      undefined,
+      functionName,
+      [],
+      [],
+      undefined,
+      ts.createBlock(
+        [
+          ts.createExpressionStatement(
+            ts.createAssignment(
+              ts.createIdentifier('const deprecated'),
+              ts.createIdentifier('process.env.DEPRECATED'),
+            ),
+          ),
+          ts.createExpressionStatement(
+            ts.createAssignment(
+              ts.createIdentifier('const deprecationMode'),
+              ts.createIdentifier(
+                "['warn', 'error', 'quiet'].includes(deprecated) ? deprecated : \"warn\"",
+              ),
+            ),
+          ),
+          ts.createExpressionStatement(
+            ts.createAssignment(
+              ts.createIdentifier('const message'),
+              ts.createLiteral(message),
+            ),
+          ),
+          // ts.createExpressionStatement(
+          ts.createSwitch(
+            ts.createIdentifier('deprecationMode'),
+            ts.createCaseBlock([
+              ts.createCaseClause(ts.createLiteral('error'), [
+                ts.createThrow(
+                  ts.createNew(
+                    ts.createIdentifier('Error'),
+                    [],
+                    [ts.createIdentifier('message')],
+                  ),
+                ),
+                ts.createBreak(),
+              ]),
+              ts.createCaseClause(ts.createLiteral('warn'), [
+                ts.createExpressionStatement(
+                  ts.createCall(
+                    ts.createIdentifier('console.warn'),
+                    [],
+                    [ts.createIdentifier("'[WARNING] ' + message")],
+                  ),
+                ),
+                ts.createBreak(),
+              ]),
+            ]),
+          ),
+        ],
+        true,
+      ),
+    ),
+    ts.createExpressionStatement(
+      ts.createCall(ts.createIdentifier(functionName), [], []),
+    ),
+  ];
 }
 
 function isMethodLikeDeclaration(node: ts.Node): boolean {
@@ -238,21 +326,29 @@ function isMethodLikeDeclaration(node: ts.Node): boolean {
 function getWarning(
   typeChecker: ts.TypeChecker,
   node: ts.Node,
-): Warning | undefined {
+  elementType: ElementType,
+): Warning {
   const original = ts.getOriginalNode(node);
-  const warnings = ts
+  const deprecatedTag = ts
     .getJSDocTags(original)
-    .filter(
-      (tag) => (tag.tagName.text ?? tag.tagName.escapedText) === 'deprecated',
-    )
-    .map((tag: ts.JSDocTag) => {
-      const type = typeChecker.getTypeAtLocation(node);
-      // TODO We need a better fully qualified name
-      const fqn = typeChecker.getFullyQualifiedName(type.symbol);
-      return {
-        elementName: fqn,
-        message: tag.comment,
-      } as Warning;
-    });
-  return warnings[0];
+    .find(
+      (tag: ts.JSDocTag) =>
+        (tag.tagName.text ?? tag.tagName.escapedText) === 'deprecated',
+    )!;
+
+  typeChecker.getTypeAtLocation(node);
+  // TODO Find a way to the get fully qualified name of any node
+  const fqn = '[NAME PLACEHOLDER]';
+
+  return {
+    elementName: fqn,
+    message: deprecatedTag.comment,
+    type: elementType,
+  } as Warning;
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
