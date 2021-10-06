434e5a6d651c0005b8f4779c0c204b5588781ccf
Otavio Macedo <otaviom@amazon.com>
2021-10-06 13:20:33 +0100

    Made test OS agnostic

--- packages/jsii/test/symbol-identifiers.test.ts
+++ packages/jsii/test/symbol-identifiers.test.ts
@@ -1,3 +1,5 @@
+import * as path from 'path';
+
 import { compileJsiiForTest } from '../lib';
 
 test('Symbol map is generated', async () => {
@@ -25,6 +27,14 @@ test('Symbol map is generated', async () => {
 
   const types = result.assembly.types ?? {};
   expect(types['testpkg.Foo'].symbolId).toEqual('index:Foo');
-  expect(types['testpkg.Bar'].symbolId).toEqual('some/nested/file:Bar');
-  expect(types['testpkg.Baz'].symbolId).toEqual('some/nested/file:Baz');
+  expect(types['testpkg.Bar'].symbolId?.split(path.sep)).toEqual([
+    'some',
+    'nested',
+    'file:Bar',
+  ]);
+  expect(types['testpkg.Baz'].symbolId?.split(path.sep)).toEqual([
+    'some',
+    'nested',
+    'file:Baz',
+  ]);
 });
