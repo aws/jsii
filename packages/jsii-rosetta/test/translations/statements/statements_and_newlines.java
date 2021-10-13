26c95f5cbcf23fe026faf3190488be2534313dd3
Rico Huijbers <rix0rrr@gmail.com>
2021-10-13 17:21:51 +0200

    fix(rosetta): newlines after return statements missing (#3063)
    Handling of newlines was incorrect (and missing a terminating
    semicolon) around lines involving `return`s.
    
    Fix that, and fix correct handling of derived function return
    types while we're at it.
    
    Fixes #3054.
    
    
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

---
+++ packages/jsii-rosetta/test/translations/statements/statements_and_newlines.java
@@ -0,0 +1,21 @@
+public Number doThing() {
+    Number x = 1; // x seems to be equal to 1
+    return x + 1;
+}
+
+public boolean doThing2(Number x) {
+    if (x == 1) {
+        return true;
+    }
+    return false;
+}
+
+public Number doThing3() {
+    Number x = 1;
+    return x + 1;
+}
+
+public void doThing4() {
+    Number x = 1;
+    x = 85;
+}
