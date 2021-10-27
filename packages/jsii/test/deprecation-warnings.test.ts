d0104495956ae02458530926b296fcc508a0622a
Nick Lynch <nlynch@amazon.com>
2021-10-27 10:32:51 +0100

    fix: deprecation-warnings includes TS-only import
    The --deprecation-warnings flag produces code which compiles to valid JS in some
    scenarios (namely, when something is exported from the same file), but not in
    others. In the latter case, the `import =` syntax causes the compiler to choke.

--- packages/jsii/test/deprecation-warnings.test.ts
+++ packages/jsii/test/deprecation-warnings.test.ts
@@ -78,8 +78,8 @@ function testpkg_Baz(p) {
         export interface Foo {}
         export interface Bar {}
         export interface Baz {
-          readonly foo: Foo; 
-          readonly bar: Bar; 
+          readonly foo: Foo;
+          readonly bar: Bar;
           readonly x: string;
         }
         `,
@@ -190,8 +190,8 @@ function testpkg_Baz(p) {
       `
         export enum State {
           ON,
-          
-          ${DEPRECATED} 
+
+          ${DEPRECATED}
           OFF
         }
         `,
@@ -239,10 +239,10 @@ function testpkg_Baz(p) {
       export interface Bar {
         readonly x: string;
       }
-      
+
       export interface Foo {
         readonly y: string;
-    
+
         /** @deprecated kkkkkkkk */
         readonly bar: Bar;
       }
@@ -317,14 +317,11 @@ describe('Call injections', () => {
     );
 
     const expectedPath = ['..', '..', '.warnings.jsii.js'].join(path.sep);
-    const requireRegex = /const jsiiDeprecationWarnings = require\("(.+)"\);/g;
-
     const content = jsFile(result, 'some/folder/source');
-    const match = requireRegex.exec(content);
-    expect(match).toBeDefined();
 
-    const actualPath = match![1];
-    expect(path.normalize(actualPath)).toEqual(expectedPath);
+    expect(content).toContain(
+      `const jsiiDeprecationWarnings = require("${expectedPath}");`,
+    );
   });
 
   test('deprecated methods', async () => {
@@ -385,7 +382,7 @@ describe('Call injections', () => {
     export class Foo {
       private _x = 0;
       public get x(){return this._x}
-      
+
       ${DEPRECATED}
       public set x(_x: number) {this._x = _x;}
     }
