7e970d8cf5317e14c4a55b58b2c70c5e5ae2346e
Rico Huijbers <rix0rrr@gmail.com>
2021-11-08 11:03:28 +0100

    Fix linter issues

--- packages/jsii-pacmak/lib/targets/index.ts
+++ packages/jsii-pacmak/lib/targets/index.ts
@@ -1,4 +1,8 @@
-import { IndependentPackageBuilder, TargetBuilder, BuildOptions } from '../builder';
+import {
+  IndependentPackageBuilder,
+  TargetBuilder,
+  BuildOptions,
+} from '../builder';
 import { JsiiModule } from '../packaging';
 import { Toposorted } from '../toposort';
 import { flatten } from '../util';
@@ -25,8 +29,10 @@ export const ALL_BUILDERS: { [key in TargetName]: BuilderFactory } = {
   dotnet: (ms, o) => new DotnetBuilder(flatten(ms), o),
   go: (ms, o) => new IndependentPackageBuilder(TargetName.GO, Golang, ms, o),
   java: (ms, o) => new JavaBuilder(flatten(ms), o),
-  js: (ms, o) => new IndependentPackageBuilder(TargetName.JAVASCRIPT, JavaScript, ms, o),
-  python: (ms, o) => new IndependentPackageBuilder(TargetName.PYTHON, Python, ms, o),
+  js: (ms, o) =>
+    new IndependentPackageBuilder(TargetName.JAVASCRIPT, JavaScript, ms, o),
+  python: (ms, o) =>
+    new IndependentPackageBuilder(TargetName.PYTHON, Python, ms, o),
 };
 
 export const INCOMPLETE_DISCLAIMER_COMPILING =
