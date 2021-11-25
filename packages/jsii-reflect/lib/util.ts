2dcb9a037e0abf341ba8cc77acc9478268102d78
Rico Huijbers <rix0rrr@gmail.com>
2021-11-25 16:44:57 +0100

    fix(jsii-reflect): improve compatibility with restricted-export modules
    Modules can decide they don't export `package.json`, and our methods
    of using `require.resolve('the-package/package.json')` to find the
    package will fail.
    
    Instead, resolve the main entry point and search upward for
    `package.json`, bypassing `require()`.

--- packages/jsii-reflect/lib/util.ts
+++ packages/jsii-reflect/lib/util.ts
@@ -1,7 +1,35 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
 export function indexBy<T>(xs: T[], f: (x: T) => string): { [key: string]: T } {
   const ret: { [key: string]: T } = {};
   for (const x of xs) {
     ret[f(x)] = x;
   }
   return ret;
+}
+
+/**
+ * Find a file up the tree given a starting directory
+ *
+ * Will return `undefined` if a package.json could not be found.
+ */
+export async function findUp(
+  directory: string,
+  fileName: string,
+): Promise<string | undefined> {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const candidatePath = path.join(directory, fileName);
+    // eslint-disable-next-line no-await-in-loop
+    if (await fs.pathExists(candidatePath)) {
+      return candidatePath;
+    }
+
+    const parent = path.dirname(directory);
+    if (parent === directory) {
+      return undefined;
+    }
+    directory = parent;
+  }
 }
