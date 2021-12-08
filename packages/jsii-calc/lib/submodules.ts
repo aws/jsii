a3d260302524e250a5a99c1bf1d3cb89a1262bdf
Rico Huijbers <rix0rrr@gmail.com>
2021-12-08 14:20:22 +0100

    Revert unnecessary change

--- packages/jsii-calc/lib/submodules.ts
+++ packages/jsii-calc/lib/submodules.ts
@@ -1,22 +1,10 @@
 import { submodule } from '@scope/jsii-calc-lib';
-import { Reflector } from '@scope/jsii-calc-lib/build/submodule';
 
 /**
  * Ensures submodule-imported types from dependencies can be used correctly.
  */
 export class UpcasingReflectable implements submodule.IReflectable {
-  /**
-   * Check that we can reference a type from a submodule using a scoped name
-   */
   public static readonly reflector = new submodule.Reflector();
-
-  /**
-   * Check that we can reference a type from a submodule if we import it directly
-   *
-   * We don't need to use the type, just referencing it and having the jsii compiler
-   * not throw an error is good enough.
-   */
-  public static readonly reflector2 = new Reflector();
 
   public constructor(private readonly delegate: Record<string, unknown>) {}
 
