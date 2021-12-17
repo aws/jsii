961bbbd28a967ffd89fdd7776b238d256c032f5c
Rico Huijbers <rix0rrr@gmail.com>
2021-12-17 11:04:41 +0100

    chore(jsii): more tests on importing
    Add more tests on importing types in various flavours and referencing
    types in various positions to convince myself that it actually works.

--- packages/jsii/test/submodules.test.ts
+++ packages/jsii/test/submodules.test.ts
@@ -58,54 +58,196 @@ test('submodules loaded from directories can have targets', async () => {
   );
 });
 
-test('can reference classes in submodules via direct import', () =>
-  TestWorkspace.withWorkspace(async (ws) => {
-    // There are 2 import styles:
-    //
-    // import { submodule } from 'lib';
-    // import * as submodule from 'lib/submodule';
-    //
-    // We need to support both import styles.
+type ImportStyle = 'directly' | 'as namespace' | 'with alias';
 
-    // Dependency that exports a submodule
-    await ws.addDependency(
-      await compileJsiiForTest({
-        'index.ts': 'export * as submodule from "./subdir"',
-        'subdir/index.ts': 'export class Foo { }',
-        'subdir/README.md': 'This is the README',
-      }),
-    );
+test.each(['directly', 'as namespace', 'with alias'] as ImportStyle[])(
+  'can reference submodule types, importing %s',
+  (importStyle) =>
+    TestWorkspace.withWorkspace(async (ws) => {
+      // There are 2 import styles:
+      //
+      // import { submodule } from 'lib';
+      // import * as submodule from 'lib/submodule';
+      //
+      // We need to support both import styles.
 
-    // Main library that imports the submodule class directly
-    const result = await compileJsiiForTest(
-      {
-        'index.ts': `
-        import { Foo } from 'testpkg/subdir';
+      // Dependency that exports a submodule
+      await ws.addDependency(
+        await compileJsiiForTest({
+          'index.ts': 'export * as submodule from "./subdir"',
+          'subdir/index.ts': [
+            'export class Foo { };',
+            'export interface FooInterface { readonly value?: string }',
+            'export interface IProtocol { readonly value?: string; }',
+          ].join('\n'),
+          'subdir/README.md': 'This is the README',
+        }),
+      );
+
+      let importStatement;
+      let prefix;
+      switch (importStyle) {
+        case 'directly':
+          importStatement =
+            "import { Foo, FooInterface, IProtocol } from 'testpkg/subdir'";
+          prefix = '';
+          break;
+        case 'as namespace':
+          importStatement = "import { submodule } from 'testpkg'";
+          prefix = 'submodule.';
+          break;
+        case 'with alias':
+          importStatement = "import { submodule as sub } from 'testpkg'";
+          prefix = 'sub.';
+          break;
+      }
+
+      // Main library that imports the submodule class directly
+      // Use the type in all possible positions
+      const result = await compileJsiiForTest(
+        {
+          'index.ts': `
+        ${importStatement};
+
+        export interface BarProps {
+          readonly foo?: ${prefix}Foo;
+        }
 
         export class Bar {
-          constructor(public readonly foo: Foo) { }
-        }
-      `,
-      },
-      {
-        packageJson: {
-          // Must be a different name from the dependency
-          name: 'consumerpkg',
-          peerDependencies: { testpkg: '*' },
-        },
-        compilationDirectory: ws.rootDirectory,
-      },
-    );
+          constructor(public readonly foo: ${prefix}Foo, props: BarProps = {}) {
+            Array.isArray(props);
+          }
 
-    expect(
-      (result.assembly.types?.['consumerpkg.Bar'] as spec.ClassType)
-        ?.initializer?.parameters,
-    ).toEqual([
-      {
-        name: 'foo',
-        type: {
-          fqn: 'testpkg.submodule.Foo',
+          public returnValue(): ${prefix}Foo {
+            return new ${prefix}Foo();
+          }
+        }
+
+        export class SubFoo extends ${prefix}Foo {}
+        export interface SubInterface extends ${prefix}FooInterface {}
+        export class Implementor implements ${prefix}IProtocol {}
+      `,
         },
-      },
-    ]);
-  }));
+        {
+          packageJson: {
+            // Must be a different name from the dependency
+            name: 'consumerpkg',
+            peerDependencies: { testpkg: '*' },
+          },
+          compilationDirectory: ws.rootDirectory,
+        },
+      );
+
+      expect(
+        (result.assembly.types?.['consumerpkg.Bar'] as spec.ClassType)
+          ?.initializer?.parameters,
+      ).toEqual([
+        {
+          name: 'foo',
+          type: { fqn: 'testpkg.submodule.Foo' },
+        },
+        {
+          name: 'props',
+          optional: true,
+          type: { fqn: 'consumerpkg.BarProps' },
+        },
+      ]);
+    }),
+);
+
+test.each(['directly', 'as namespace', 'with alias'] as ImportStyle[])(
+  'can reference nested types in submodules, importing %s',
+  (importStyle) =>
+    TestWorkspace.withWorkspace(async (ws) => {
+      // There are 2 import styles:
+      //
+      // import { submodule } from 'lib';
+      // import * as submodule from 'lib/submodule';
+      //
+      // We need to support both import styles.
+
+      // Dependency that exports a submodule
+      await ws.addDependency(
+        await compileJsiiForTest({
+          'index.ts': 'export * as submodule from "./subdir"',
+          'subdir/index.ts': [
+            'export class Namespace {};',
+            'export namespace Namespace {',
+            '  export class Foo { };',
+            '  export interface FooInterface { readonly value?: string }',
+            '  export interface IProtocol { readonly value?: string; }',
+            '}',
+          ].join('\n'),
+          'subdir/README.md': 'This is the README',
+        }),
+      );
+
+      let importStatement;
+      let prefix;
+      switch (importStyle) {
+        case 'directly':
+          importStatement = "import { Namespace } from 'testpkg/subdir'";
+          prefix = 'Namespace.';
+          break;
+        case 'as namespace':
+          importStatement = "import { submodule } from 'testpkg'";
+          prefix = 'submodule.Namespace.';
+          break;
+        case 'with alias':
+          importStatement = "import { submodule as sub } from 'testpkg'";
+          prefix = 'sub.Namespace.';
+          break;
+      }
+
+      // Main library that imports the submodule class directly
+      // Use the type in all possible positions
+      const result = await compileJsiiForTest(
+        {
+          'index.ts': `
+        ${importStatement};
+
+        export interface BarProps {
+          readonly foo?: ${prefix}Foo;
+        }
+
+        export class Bar {
+          constructor(public readonly foo: ${prefix}Foo, props: BarProps = {}) {
+            Array.isArray(props);
+          }
+
+          public returnValue(): ${prefix}Foo {
+            return new ${prefix}Foo();
+          }
+        }
+
+        export class SubFoo extends ${prefix}Foo {}
+        export interface SubInterface extends ${prefix}FooInterface {}
+        export class Implementor implements ${prefix}IProtocol {}
+      `,
+        },
+        {
+          packageJson: {
+            // Must be a different name from the dependency
+            name: 'consumerpkg',
+            peerDependencies: { testpkg: '*' },
+          },
+          compilationDirectory: ws.rootDirectory,
+        },
+      );
+
+      expect(
+        (result.assembly.types?.['consumerpkg.Bar'] as spec.ClassType)
+          ?.initializer?.parameters,
+      ).toEqual([
+        {
+          name: 'foo',
+          type: { fqn: 'testpkg.submodule.Namespace.Foo' },
+        },
+        {
+          name: 'props',
+          optional: true,
+          type: { fqn: 'consumerpkg.BarProps' },
+        },
+      ]);
+    }),
+);
