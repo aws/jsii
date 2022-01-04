e174a5a330ee2b10c70c2d87fdc2b7ab36e87ccc
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2022-01-04 15:32:58 +0100

    feat(go): add UnsafeCast function
    The `UnsafeCast` function can be used to forcefully convert from one
    type of jsii proxy value (including `interface{}`) to another jsii
    interface (i.e: a class or interface instance).
    
    If performs a "clean" cast when possible, and creates a new, aliased
    proxy otherwise. It is the user's responsibility to ensure they are
    passing the correct arguments into the function.

---
+++ packages/@jsii/go-runtime/jsii-runtime-go/cast.go
@@ -0,0 +1,53 @@
+package jsii
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/aws/jsii-runtime-go/internal/kernel"
+)
+
+// UnsafeCast converts the given interface value to the desired target interface
+// pointer. Panics if the from value is not a jsii proxy object, or if the to
+// value is not a pointer to an interface type.
+func UnsafeCast(from interface{}, into interface{}) {
+	rinto := reflect.ValueOf(into)
+	if rinto.Kind() != reflect.Ptr {
+		panic(fmt.Errorf("Second argument to UnsafeCast must be a pointer to an interface. Received %s", rinto.Type().String()))
+	}
+	rinto = rinto.Elem()
+	if rinto.Kind() != reflect.Interface {
+		panic(fmt.Errorf("Second argument to UnsafeCast must be a pointer to an interface. Received pointer to %s", rinto.Type().String()))
+	}
+
+	rfrom := reflect.ValueOf(from)
+
+	// If rfrom is essentially nil, set into to nil and return.
+	if !rfrom.IsValid() || rfrom.IsZero() {
+		null := reflect.Zero(rinto.Type())
+		rinto.Set(null)
+		return
+	}
+	// Interfaces may present as a pointer to an implementing struct, and that's fine...
+	if rfrom.Kind() != reflect.Interface && rfrom.Kind() != reflect.Ptr {
+		panic(fmt.Errorf("First argument to UnsafeCast must be an interface value. Received %s", rfrom.Type().String()))
+	}
+
+	// If rfrom can be directly converted to rinto, just do it.
+	if rfrom.CanConvert(rinto.Type()) {
+		rfrom = rfrom.Convert(rinto.Type())
+		rinto.Set(rfrom)
+		return
+	}
+
+	client := kernel.GetClient()
+	if objID, found := client.FindObjectRef(rfrom); found {
+		// Ensures the value is initialized properly. Panics if the target value is not a jsii interface type.
+		client.Types().InitJsiiProxy(rinto)
+		// Make the new value an alias to the old value.
+		client.RegisterInstance(rinto, objID)
+		return
+	}
+
+	panic(fmt.Errorf("First argument to UnsafeCast must be a jsii proxy value. Received %s", rfrom.String()))
+}
