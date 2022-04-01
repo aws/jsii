64d398c9f9610afc375c5a65a6c8dfb68021b66e
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2022-03-31 19:00:47 -0700

    remove some more redundant asyncs

--- packages/jsii-diff/test/util.ts
+++ packages/jsii-diff/test/util.ts
@@ -4,25 +4,25 @@ import * as reflect from 'jsii-reflect';
 import { compareAssemblies } from '../lib';
 import { Mismatches } from '../lib/types';
 
-export async function expectNoError(original: string, updated: string) {
-  const mms = await compare(original, updated);
+export function expectNoError(original: string, updated: string) {
+  const mms = compare(original, updated);
   for (const msg of mms.messages()) {
     console.error(`- ${msg}`);
   }
   expect(mms.count).toBe(0);
 }
 
-export async function expectError(
+export function expectError(
   error: RegExp | undefined,
   original: string,
   updated: string,
 ) {
   if (error == null) {
-    await expectNoError(original, updated);
+    expectNoError(original, updated);
     return;
   }
 
-  const mms = await compare(original, updated);
+  const mms = compare(original, updated);
   expect(mms.count).not.toBe(0);
 
   const msgs = Array.from(mms.messages());
@@ -32,23 +32,14 @@ export async function expectError(
   }
 }
 
-export async function compare(
-  original: string,
-  updated: string,
-): Promise<Mismatches> {
+export function compare(original: string, updated: string): Mismatches {
   const ass1 = sourceToAssemblyHelper(original);
-  await expect(ass1).resolves.not.toThrowError();
   const ts1 = new reflect.TypeSystem();
-  const originalAssembly = ts1.addAssembly(
-    new reflect.Assembly(ts1, await ass1),
-  );
+  const originalAssembly = ts1.addAssembly(new reflect.Assembly(ts1, ass1));
 
   const ass2 = sourceToAssemblyHelper(updated);
-  await expect(ass2).resolves.not.toThrowError();
   const ts2 = new reflect.TypeSystem();
-  const updatedAssembly = ts2.addAssembly(
-    new reflect.Assembly(ts2, await ass2),
-  );
+  const updatedAssembly = ts2.addAssembly(new reflect.Assembly(ts2, ass2));
 
   return compareAssemblies(originalAssembly, updatedAssembly);
 }
