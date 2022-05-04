fc1cb6a7ff8990f7ded49d951ee946c9abb51638
Mitchell <mitchellvaline@yahoo.com>
2022-05-04 09:23:42 -0700

    pr feedback

--- packages/@jsii/benchmarks/lib/constants.ts
+++ packages/@jsii/benchmarks/lib/constants.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 
-export const fixturesDir = path.join(__dirname, '..', 'fixtures');
+export const fixturesDir = path.resolve(__dirname, '..', 'fixtures');
 
 export const cdkTagv2_21_1 = 'v2.21.1';
 export const cdkv2_21_1 = path.resolve(
