0068b7feaf2a3866b24485debb08855d33c3e750
Kaizen Conroy <zen.conroy@gmail.com>
2022-06-02 18:49:40 -0400

    new module for jsii utility functions

---
+++ packages/@jsii/utils/lib/assembly.ts
@@ -0,0 +1,98 @@
+import * as spec from '@jsii/spec';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import * as zlib from 'zlib';
+
+export const SPEC_FILE_NAME = '.jsii';
+export const SPEC_FILE_NAME_COMPRESSED = `${SPEC_FILE_NAME}.gz`;
+
+/**
+ * Finds the path to the assembly file, which will be
+ * '.jsii' if uncompressed and '.jsii.gz' if compressed.
+ *
+ * @param directory path to a directory with an assembly file
+ * @returns path to the assembly file
+ */
+export function getAssemblyFile(directory: string) {
+  const compressedJsiiFile = path.join(directory, SPEC_FILE_NAME_COMPRESSED);
+  const compressedJsiiExists = fs.existsSync(compressedJsiiFile);
+
+  const uncompressedJsiiFile = path.join(directory, SPEC_FILE_NAME);
+  const uncompressedJsiiExists = fs.existsSync(uncompressedJsiiFile);
+
+  if (!compressedJsiiExists && !uncompressedJsiiExists) {
+    throw new Error(
+      `No ${SPEC_FILE_NAME} or ${SPEC_FILE_NAME_COMPRESSED} assembly file was found at ${directory}`,
+    );
+  }
+
+  return compressedJsiiExists ? compressedJsiiFile : uncompressedJsiiFile;
+}
+
+/**
+ * Writes the assembly file either as .jsii or .jsii.gz if zipped
+ *
+ * @param directory the directory path to place the assembly file
+ * @param assembly the contents of the assembly
+ * @param zip whether or not to zip the assembly (.jsii.gz)
+ * @returns whether or not the assembly was zipped
+ */
+export function writeAssembly(
+  directory: string,
+  assembly: spec.Assembly,
+  zip: boolean,
+) {
+  if (zip) {
+    // TODO: also write .jsii
+    fs.writeFileSync(
+      path.join(directory, SPEC_FILE_NAME_COMPRESSED),
+      zlib.gzipSync(JSON.stringify(assembly)),
+    );
+  } else {
+    fs.writeJsonSync(path.join(directory, SPEC_FILE_NAME), assembly);
+  }
+
+  return zip;
+}
+
+/**
+ * Loads the assembly file and unzips compressed assemblies.
+ *
+ * @param directory the directory of the assembly file
+ * @returns the assembly file as json
+ */
+export function loadAssemblyFromPath(directory: string) {
+  const assemblyFile = getAssemblyFile(directory);
+  return loadAssemblyFromFile(assemblyFile);
+}
+
+/**
+ * Loads the assembly file and unzips compressed assemblies.
+ *
+ * @param pathToFile the path to the assembly file
+ * @returns the assembly file as json
+ */
+export function loadAssemblyFromFile(pathToFile: string) {
+  const extname = path.extname(pathToFile);
+  if (extname === '.gz') {
+    return readZippedAssembly(pathToFile);
+  } else if (extname === '') {
+    return readAssembly(pathToFile);
+  }
+
+  throw new Error(
+    `Assembly file must be named ${SPEC_FILE_NAME} or ${SPEC_FILE_NAME_COMPRESSED} but got ${path.basename(
+      pathToFile,
+    )}`,
+  );
+}
+
+function readAssembly(pathToFile: string) {
+  return fs.readJsonSync(pathToFile, {
+    encoding: 'utf-8',
+  });
+}
+
+function readZippedAssembly(pathToFile: string) {
+  return JSON.parse(zlib.gunzipSync(fs.readFileSync(pathToFile)).toString());
+}
---
+++ packages/@jsii/utils/test/assembly.test.ts
@@ -0,0 +1,111 @@
+import * as spec from '@jsii/spec';
+import * as fs from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
+
+import {
+  loadAssemblyFromPath,
+  getAssemblyFile,
+  writeAssembly,
+  SPEC_FILE_NAME,
+  SPEC_FILE_NAME_COMPRESSED,
+} from '../lib';
+
+const TEST_ASSEMBLY: spec.Assembly = {
+  schema: spec.SchemaVersion.LATEST,
+  name: 'jsii-test-dep',
+  version: '1.2.4',
+  license: 'Apache-2.0',
+  description: 'A test assembly',
+  homepage: 'https://github.com/aws/jsii',
+  repository: { type: 'git', url: 'git://github.com/aws/jsii.git' },
+  author: {
+    name: 'Amazon Web Services',
+    url: 'https://aws.amazon.com',
+    organization: true,
+    roles: ['author'],
+  },
+  fingerprint: 'F1NG3RPR1N7',
+  dependencies: {
+    'jsii-test-dep-dep': '3.2.1',
+  },
+  jsiiVersion: '1.0.0',
+};
+
+describe('writeAssembly', () => {
+  test('can write compressed assembly', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, true);
+
+    expect(
+      fs.existsSync(path.join(tmpdir, SPEC_FILE_NAME_COMPRESSED)),
+    ).toBeTruthy();
+  });
+
+  test('can write uncompressed assembly', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, false);
+
+    expect(fs.existsSync(path.join(tmpdir, SPEC_FILE_NAME))).toBeTruthy();
+  });
+});
+
+describe('getAssemblyFile', () => {
+  test('finds compressed assembly file', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, true);
+
+    expect(getAssemblyFile(tmpdir)).toEqual(
+      path.join(tmpdir, SPEC_FILE_NAME_COMPRESSED),
+    );
+  });
+
+  test('finds uncompressed assembly file', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, false);
+
+    expect(getAssemblyFile(tmpdir)).toEqual(path.join(tmpdir, SPEC_FILE_NAME));
+  });
+
+  test('prefers compressed file over uncompressed file', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, true);
+    writeAssembly(tmpdir, TEST_ASSEMBLY, false);
+
+    expect(getAssemblyFile(tmpdir)).toEqual(
+      path.join(tmpdir, SPEC_FILE_NAME_COMPRESSED),
+    );
+  });
+});
+
+describe('loadAssemblyFromPath', () => {
+  test('loads compressed assembly', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, true);
+
+    expect(loadAssemblyFromPath(tmpdir)).toEqual(TEST_ASSEMBLY);
+  });
+
+  test('loads uncompressed assembly', () => {
+    const tmpdir = makeTempDir();
+    writeAssembly(tmpdir, TEST_ASSEMBLY, false);
+
+    expect(loadAssemblyFromPath(tmpdir)).toEqual(TEST_ASSEMBLY);
+  });
+
+  test('compressed and uncompressed assemblies are loaded identically', () => {
+    const compressedTmpDir = makeTempDir();
+    const uncompressedTmpDir = makeTempDir();
+
+    writeAssembly(compressedTmpDir, TEST_ASSEMBLY, true);
+    writeAssembly(uncompressedTmpDir, TEST_ASSEMBLY, false);
+
+    expect(loadAssemblyFromPath(compressedTmpDir)).toEqual(
+      loadAssemblyFromPath(uncompressedTmpDir),
+    );
+  });
+});
+
+function makeTempDir() {
+  return fs.mkdtempSync(path.join(os.tmpdir(), path.basename(__filename)));
+}
