054736092ebf99e6d3df70a205ba509d54e0d9c7
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2022-06-03 13:28:08 +0200

    chore(codemaker): build out-of-tree

--- packages/codemaker/src/filebuff.test.ts
+++ packages/codemaker/src/filebuff.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs-extra';
 import * as os from 'os';
 import * as path from 'path';
 
-import FileBuffer from '../lib/filebuff';
+import FileBuffer from './filebuff';
 
 test('file buffer', async () => {
   const fb = new FileBuffer('hello.source');
