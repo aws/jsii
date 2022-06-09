a6487fdaa8da4c6aa43efaa9cf1000c16569909d
github-action-benchmark <github@users.noreply.github.com>
2022-06-09 10:31:24 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for de3fc03f085840610a7fca428d6aa39f6e934b00

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654716736469,
+  "lastUpdate": 1654770684128,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -1457,6 +1457,37 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 0,
             "extra": "Compile aws-cdk-lib@v2.21.1 averaged 86662.932948 milliseconds over 1 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "de3fc03f085840610a7fca428d6aa39f6e934b00",
+          "message": "chore(deps): Bump github.com/stretchr/testify from 1.7.1 to 1.7.2 in /packages/@jsii/go-runtime/jsii-runtime-go (#3575)\n\nBumps [github.com/stretchr/testify](https://github.com/stretchr/testify) from 1.7.1 to 1.7.2.\n<details>\n<summary>Commits</summary>\n<ul>\n<li><a href=\"https://github.com/stretchr/testify/commit/41453c009af9a942261b7a25a88521d0d6804e7f\"><code>41453c0</code></a> Update gopkg.in/yaml.v3</li>\n<li><a href=\"https://github.com/stretchr/testify/commit/285adcc5ced0bb267a7c874cfa3ca238266ce14f\"><code>285adcc</code></a> Update go versions in build matrix</li>\n<li><a href=\"https://github.com/stretchr/testify/commit/6e7fab43fc3294f4e7c079e260746edcf8d66639\"><code>6e7fab4</code></a> Bump actions/setup-go from 2 to 3.1.0</li>\n<li><a href=\"https://github.com/stretchr/testify/commit/106ec21d14df07d4c33ec1155328800300c28c7f\"><code>106ec21</code></a> use RWMutex</li>\n<li><a href=\"https://github.com/stretchr/testify/commit/a409ccf19e17d8a90156521ce71c5e8ef8f6bca8\"><code>a409ccf</code></a> fix data race in the suit</li>\n<li><a href=\"https://github.com/stretchr/testify/commit/35864782d21250c3ff66874e87e5fdc9c79e0be1\"><code>3586478</code></a> assert: fix typo</li>\n<li><a href=\"https://github.com/stretchr/testify/commit/77977386932ab1866a4b9556a7af1ae347531786\"><code>7797738</code></a> Update versions supported to include <code>go 1.16</code></li>\n<li>See full diff in <a href=\"https://github.com/stretchr/testify/compare/v1.7.1...v1.7.2\">compare view</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility score](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=github.com/stretchr/testify&package-manager=go_modules&previous-version=1.7.1&new-version=1.7.2)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't alter it yourself. You can also trigger a rebase manually by commenting `@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits that have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after your CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge and block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating it. You can achieve the same result by closing it manually\n- `@dependabot ignore this major version` will close this PR and stop Dependabot creating any more for this major version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this minor version` will close this PR and stop Dependabot creating any more for this minor version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this dependency` will close this PR and stop Dependabot creating any more for this dependency (unless you reopen the PR or upgrade to it yourself)\n\n\n</details>",
+          "timestamp": "2022-06-09T10:12:50Z",
+          "tree_id": "6260c1d637c92755a8220bc0b98fb96a7e074428",
+          "url": "https://github.com/aws/jsii/commit/de3fc03f085840610a7fca428d6aa39f6e934b00"
+        },
+        "date": 1654770680900,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.21.1",
+            "value": 77938.341484,
+            "unit": "milliseconds",
+            "range": 0,
+            "extra": "Compile aws-cdk-lib@v2.21.1 averaged 77938.341484 milliseconds over 1 runs"
           }
         ]
       }
