833a71781c9e4fee1522d75e53d3de76b93439f5
Romain Marcadier <rmuller@amazon.fr>
2022-07-29 17:04:57 +0200

    fix(pyhon): under-qualified types used by dynamic type checking (#3688)
    The types must be referenced from the current module's root, including
    nesting class names, or the names will not be defined.

--- packages/jsii-rosetta/jest.config.mjs
+++ packages/jsii-rosetta/jest.config.mjs
@@ -1,6 +1,7 @@
 import { createRequire } from 'node:module';
-import { overriddenConfig } from '../../jest.config.mjs';
+import { default as defaultConfig, overriddenConfig } from '../../jest.config.mjs';
 
 export default overriddenConfig({
   setupFiles: [createRequire(import.meta.url).resolve('./jestsetup.js')],
+  testTimeout: process.env.CI ? 30_000 : defaultConfig.testTimeout,
 });
