9cfaf895b988640ac18db1408d9d55d87a7f5099
Calvin Combs <comcalvi@amazon.com>
2022-08-11 16:50:20 -0700

    dead code removal

--- packages/@jsii/java-runtime-test/project/src/test/java/software/amazon/jsii/testing/TypeCheckingTest.java
+++ packages/@jsii/java-runtime-test/project/src/test/java/software/amazon/jsii/testing/TypeCheckingTest.java
@@ -1,22 +1,7 @@
 package software.amazon.jsii.testing;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import software.amazon.jsii.JsiiClient;
 import software.amazon.jsii.JsiiException;
-import software.amazon.jsii.JsiiObjectMapper;
-import software.amazon.jsii.JsiiObjectRef;
-import software.amazon.jsii.JsiiPromise;
-import software.amazon.jsii.JsiiRuntime;
-import software.amazon.jsii.JsiiSerializable;
-import software.amazon.jsii.api.Callback;
-import software.amazon.jsii.api.JsiiOverride;
 //import software.amazon.jsii.tests.compliance;
 import software.amazon.jsii.tests.calculator.*;
 
@@ -29,18 +14,6 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TypeCheckingTest {
-    @BeforeEach
-    /*public void setUp() {
-        jsiiRuntime = new JsiiRuntime();
-        this.client = jsiiRuntime.getClient();
-
-        this.client.loadModule(new software.amazon.jsii.tests.calculator.baseofbase.$Module());
-        this.client.loadModule(new software.amazon.jsii.tests.calculator.base.$Module());
-        this.client.loadModule(new software.amazon.jsii.tests.calculator.lib.$Module());
-        this.client.loadModule(new software.amazon.jsii.tests.calculator.$Module());
-    }
-    */
-
     @Test
     public void Constructor() {
         boolean thrown = false;
