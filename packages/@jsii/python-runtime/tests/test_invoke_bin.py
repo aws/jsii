98fb9dfe31910a81e7a7becdd3903766cbe7576e
Jeff Malins <jeff@curisium.com>
2022-09-21 16:18:21 -0400

    chore(python): invoke bin test

--- packages/@jsii/python-runtime/tests/test_invoke_bin.py
+++ packages/@jsii/python-runtime/tests/test_invoke_bin.py
@@ -2,11 +2,15 @@ import subprocess
 
 class TestInvokeBinScript:
     def test_invoke_script(self) -> None:
-      result = subprocess.run(".env/bin/calc", capture_output=True)
-      # FIXME
-      assert result is None
+        result = subprocess.run(".env/bin/calc", capture_output=True)
 
-    # def test_invoke_script_with_args(self) -> None:
-    #   result = subprocess.run([".env/bin/calc", "arg1", "arg2"], capture_output=True)
-    #   # FIXME
-    #   assert result is None
+        assert result.returncode == 0
+        assert result.stdout == b"Hello World!\n\n"
+        assert result.stderr == b""
+
+    def test_invoke_script_with_args(self) -> None:
+        result = subprocess.run([".env/bin/calc", "arg1", "arg2"], capture_output=True)
+
+        assert result.returncode == 0
+        assert result.stdout == b"Hello World!\n  arguments: arg1, arg2\n\n"
+        assert result.stderr == b""
