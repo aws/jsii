fd106375ec847f91b6bf4cb0ce154f277dd1165b
Jeff Malins <jeff@curisium.com>
2022-09-22 09:58:31 -0400

    chore(python): second pass at windows tests

--- packages/@jsii/python-runtime/tests/test_invoke_bin.py
+++ packages/@jsii/python-runtime/tests/test_invoke_bin.py
@@ -4,24 +4,24 @@ import subprocess
 
 class TestInvokeBinScript:
     def test_invoke_script(self) -> None:
-        script_path = get_script_path("calc")
-        result = subprocess.run(script_path, capture_output=True)
+        result = run_script("calc")
 
         assert result.returncode == 0
         assert result.stdout == b"Hello World!\n\n"
         assert result.stderr == b""
 
     def test_invoke_script_with_args(self) -> None:
-        script_path = get_script_path("calc")
-        result = subprocess.run([script_path, "arg1", "arg2"], capture_output=True)
+        result = run_script("calc", "arg1", "arg2")
 
         assert result.returncode == 0
         assert result.stdout == b"Hello World!\n  arguments: arg1, arg2\n\n"
         assert result.stderr == b""
 
 
-def get_script_path(script_name: str) -> str:
-    if platform.system == "Windows":
-        return f".env\\Scripts\\{script_name}.cmd"
+def run_script(script_name: str, *args: str) -> subprocess.CompletedProcess:
+    if platform.system() == "Windows":
+        script_path = f".env\\Scripts\\{script_name}.cmd"
+        return subprocess.run([script_path, *args], capture_output=True, shell=True)
     else:
-        return f".env/bin/{script_name}"
+        script_path = f".env/bin/{script_name}"
+        return subprocess.run([script_path, *args], capture_output=True)
