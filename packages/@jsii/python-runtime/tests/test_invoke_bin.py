2558d966bb0822276d59d421b4417c63c1ebea8d
Jeff Malins <jeff@curisium.com>
2022-09-22 08:34:31 -0400

    chore(python): fix windows invoke tests

--- packages/@jsii/python-runtime/tests/test_invoke_bin.py
+++ packages/@jsii/python-runtime/tests/test_invoke_bin.py
@@ -1,17 +1,27 @@
+import platform
 import subprocess
 
 
 class TestInvokeBinScript:
     def test_invoke_script(self) -> None:
-        result = subprocess.run(".env/bin/calc", capture_output=True)
+        script_path = get_script_path("calc")
+        result = subprocess.run(script_path, capture_output=True)
 
         assert result.returncode == 0
         assert result.stdout == b"Hello World!\n\n"
         assert result.stderr == b""
 
     def test_invoke_script_with_args(self) -> None:
-        result = subprocess.run([".env/bin/calc", "arg1", "arg2"], capture_output=True)
+        script_path = get_script_path("calc")
+        result = subprocess.run([script_path, "arg1", "arg2"], capture_output=True)
 
         assert result.returncode == 0
         assert result.stdout == b"Hello World!\n  arguments: arg1, arg2\n\n"
         assert result.stderr == b""
+
+
+def get_script_path(script_name: str) -> str:
+    if platform.system == "Windows":
+        return f".env\\Scripts\\{script_name}.cmd"
+    else:
+        return f".env/bin/{script_name}"
