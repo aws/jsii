aws/jsii

commit 5413720a274fbcc9d3b62823197b76589f99329c

fix(python): classproperty not working with type checkers (#3694)
The `jsii.python` submodule was not re-exported in `__init__.py`, which
made type resolves not manage to access the. `@classproperty` decorator
and default to treating it as `Any`.

This adds some missing type annotations, runs `pyright` on the runtime
library and tests thereof (as `mypy` did not identify this problem),
and adds the missing export declaration.

Fixes #3633



---

By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].

[Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

packages/@jsii/python-runtime/src/jsii/_runtime.py
@@ -3,7 +3,7 @@ import os
 
 import attr
 
-from typing import cast, Any, Callable, ClassVar, List, Optional, Mapping, Type, TypeVar
+from typing import cast, Any, Callable, List, Optional, Mapping, Type, TypeVar
 
 from . import _reference_map
 from ._compat import importlib_resources
@@ -49,7 +49,7 @@ class JSIIAssembly:
     def invokeBinScript(
         cls, pkgname: str, script: str, *args: str, _kernel=kernel
     ) -> None:
-        response = _kernel.invokeBinScript(pkgname, script, *args)
+        response = _kernel.invokeBinScript(pkgname, script, args)
         print(response.stdout)
 
 
@@ -154,7 +154,7 @@ def proxy_for(abstract_class: Type[Any]) -> Type[Any]:
     if not hasattr(abstract_class, "__jsii_proxy_class__"):
         raise TypeError(f"{abstract_class} is not a JSII Abstract class.")
 
-    return abstract_class.__jsii_proxy_class__()
+    return cast(Any, abstract_class).__jsii_proxy_class__()
 
 
 def python_jsii_mapping(cls: Type[Any]) -> Optional[Mapping[str, str]]:

packages/jsii-pacmak/lib/targets/python.ts
@@ -627,7 +627,6 @@ abstract class BaseMethod implements PythonBase {
     const decorators = new Array<string>();
 
     if (this.jsName !== undefined) {
-      // "# type: ignore[misc]" needed because mypy does not know how to check decorated declarations
       decorators.push(`@jsii.member(jsii_name="${this.jsName}")`);
     }
 
@@ -640,10 +639,7 @@ abstract class BaseMethod implements PythonBase {
     }
 
     if (decorators.length > 0) {
-      // "# type: ignore[misc]" needed because mypy does not know how to check decorated declarations
-      for (const decorator of decorators
-        .join(' # type: ignore[misc]\n')
-        .split('\n')) {
+      for (const decorator of decorators) {
         code.line(decorator);
       }
     }
@@ -655,14 +651,7 @@ abstract class BaseMethod implements PythonBase {
       ),
     );
 
-    openSignature(
-      code,
-      'def',
-      this.pythonName,
-      pythonParams,
-      false,
-      returnType,
-    );
+    openSignature(code, 'def', this.pythonName, pythonParams, returnType);
     this.generator.emitDocString(code, this.apiLocation, this.docs, {
       arguments: documentableArgs,
       documentableItem: `method-${this.pythonName}`,
@@ -895,8 +884,7 @@ abstract class BaseProperty implements PythonBase {
     const { renderAbstract = true, forceEmitBody = false } = opts ?? {};
     const pythonType = toTypeName(this.type).pythonType(context);
 
-    // "# type: ignore[misc]" is needed because mypy cannot check decorated things
-    code.line(`@${this.decorator} # type: ignore[misc]`);
+    code.line(`@${this.decorator}`);
     code.line(`@jsii.member(jsii_name="${this.jsName}")`);
     if (renderAbstract && this.abstract) {
       code.line('@abc.abstractmethod');
@@ -906,8 +894,12 @@ abstract class BaseProperty implements PythonBase {
       'def',
       this.pythonName,
       [this.implicitParameter],
-      true,
       pythonType,
+      // PyRight and MyPY both special-case @property, but not custom implementations such as our @classproperty...
+      // MyPY reports on the re-declaration, but PyRight reports on the initial declaration (duh!)
+      this.isStatic && !this.immutable
+        ? 'pyright: ignore [reportGeneralTypeIssues]'
+        : undefined,
     );
     this.generator.emitDocString(code, this.apiLocation, this.docs, {
       documentableItem: `prop-${this.pythonName}`,
@@ -927,6 +919,8 @@ abstract class BaseProperty implements PythonBase {
 
     if (!this.immutable) {
       code.line();
+      // PyRight and MyPY both special-case @property, but not custom implementations such as our @classproperty...
+      // MyPY reports on the re-declaration, but PyRight reports on the initial declaration (duh!)
       code.line(
         `@${this.pythonName}.setter${
           this.isStatic ? ' # type: ignore[no-redef]' : ''
@@ -940,7 +934,6 @@ abstract class BaseProperty implements PythonBase {
         'def',
         this.pythonName,
         [this.implicitParameter, `value: ${pythonType}`],
-        false,
         'None',
       );
       if (
@@ -1129,16 +1122,16 @@ class Struct extends BasePythonClassType {
         ? [implicitParameter, '*', ...kwargs]
         : [implicitParameter];
 
-    openSignature(code, 'def', '__init__', constructorArguments, false, 'None');
+    openSignature(code, 'def', '__init__', constructorArguments, 'None');
     this.emitConstructorDocstring(code);
 
     // Re-type struct arguments that were passed as "dict". Do this before validating argument types...
     for (const member of members.filter((m) => m.isStruct(this.generator))) {
       // Note that "None" is NOT an instance of dict (that's convenient!)
-      const typeName = toTypeName(member.type.type).pythonType({
-        ...context,
-        typeAnnotation: false,
-      });
+      const typeName = toPythonFullName(
+        (member.type.type as spec.NamedTypeReference).fqn,
+        context.assembly,
+      );
       code.openBlock(`if isinstance(${member.pythonName}, dict)`);
       code.line(`${member.pythonName} = ${typeName}(**${member.pythonName})`);
       code.closeBlock();
@@ -1193,7 +1186,7 @@ class Struct extends BasePythonClassType {
     const pythonType = member.typeAnnotation(context);
 
     code.line('@builtins.property');
-    openSignature(code, 'def', member.pythonName, ['self'], true, pythonType);
+    openSignature(code, 'def', member.pythonName, ['self'], pythonType);
     member.emitDocString(code);
     // NOTE: No parameter to validate here, this is a getter.
     code.line(
@@ -2170,6 +2163,12 @@ class Package {
       );
     code.line(`requires = [${buildTools.map((x) => `"${x}"`).join(', ')}]`);
     code.line('build-backend = "setuptools.build_meta"');
+    code.line();
+    code.line('[tool.pyright]');
+    code.line('defineConstant = { DEBUG = true }');
+    code.line('pythonVersion = "3.7"');
+    code.line('pythonPlatform = "All"');
+    code.line('reportSelfClsParameterName = false');
     code.closeFile('pyproject.toml');
 
     // We also need to write out a MANIFEST.in to ensure that all of our required
@@ -2994,16 +2993,16 @@ function openSignature(
   keyword: 'def',
   name: string,
   params: readonly string[],
-  trailingComma: boolean,
   returnType: string,
+  comment?: string,
 ): void;
 function openSignature(
   code: CodeMaker,
   keyword: 'class' | 'def',
   name: string,
   params: readonly string[],
-  trailingComma = false,
   returnType?: string,
+  lineComment?: string,
 ) {
   const prefix = `${keyword} ${name}`;
   const suffix = returnType ? ` -> ${returnType}` : '';
@@ -3015,10 +3014,10 @@ function openSignature(
   const join = ', ';
   const { elementsSize, joinSize } = totalSizeOf(params, join);
 
-  const hasComments = !params.some((param) => /# .+$/.exec(param));
+  const hasComments = params.some((param) => /#\s*.+$/.exec(param) != null);
 
   if (
-    hasComments &&
+    !hasComments &&
     TARGET_LINE_LENGTH >
       code.currentIndentLength +
         prefix.length +
@@ -3027,27 +3026,20 @@ function openSignature(
         suffix.length +
         2
   ) {
-    code.openBlock(`${prefix}(${params.join(join)})${suffix}`);
+    code.indent(
+      `${prefix}(${params.join(join)})${suffix}:${
+        lineComment ? `  # ${lineComment}` : ''
+      }`,
+    );
     return;
   }
 
   code.indent(`${prefix}(`);
-  if (
-    !hasComments &&
-    TARGET_LINE_LENGTH >
-      code.currentIndentLength +
-        elementsSize +
-        joinSize +
-        (trailingComma ? 1 : 0)
-  ) {
-    code.line(`${params.join(join)}${trailingComma ? ',' : ''}`);
-  } else {
-    for (const param of params) {
-      code.line(param.replace(/(\s*# .+)?$/, ',$1'));
-    }
+  for (const param of params) {
+    code.line(param.replace(/(\s*# .+)?$/, ',$1'));
   }
   code.unindent(false);
-  code.openBlock(`)${suffix}`);
+  code.indent(`)${suffix}:${lineComment ? `  # ${lineComment}` : ''}`);
 }
 
 /**
@@ -3063,7 +3055,7 @@ function emitParameterTypeChecks(
   typedEntity: string,
 ): void {
   const paramInfo = params.map((param) => {
-    const [name] = param.split(/\s*[:=]\s*/, 1);
+    const [name] = param.split(/\s*[:=#]\s*/, 1);
     if (name === '*') {
       return { kwargsMark: true };
     } else if (name.startsWith('*')) {
@@ -3094,14 +3086,17 @@ function emitParameterTypeChecks(
     }
 
     let expectedType = `${typesVar}[${JSON.stringify(name)}]`;
+    let comment = '';
     if (is_rest) {
       // This is a vararg, so the value will appear as a tuple.
       expectedType = `typing.Tuple[${expectedType}, ...]`;
+      // Need to ignore reportGeneralTypeIssues because pyright incorrectly parses that as a type annotation 😒
+      comment = ' # pyright: ignore [reportGeneralTypeIssues]';
     }
     code.line(
       `check_type(argname=${JSON.stringify(
         `argument ${name}`,
-      )}, value=${name}, expected_type=${expectedType})`,
+      )}, value=${name}, expected_type=${expectedType})${comment}`,
     );
   }
   if (openedBlock) {

packages/@jsii/python-runtime/pyproject.toml
@@ -9,3 +9,8 @@ exclude = '\.(git|mypy_cache|env)'
 
 [tool.mypy]
 ignore_missing_imports = true
+
+[tool.pyright]
+pythonVersion = "3.7"
+venv = ".env"
+venvPath = "."

packages/@jsii/python-runtime/src/jsii/__init__.py
@@ -14,6 +14,7 @@ from ._runtime import (
     kernel,
     proxy_for,
 )
+from . import python
 
 
 # JS doesn't have distinct float or integer types, but we do. So we'll define our own
@@ -70,4 +71,5 @@ __all__ = [
     "ainvoke",
     "sinvoke",
     "stats",
+    "python",
 ]

packages/@jsii/python-runtime/src/jsii/_kernel/__init__.py
@@ -3,7 +3,7 @@ import inspect
 import itertools
 from types import FunctionType, MethodType, BuiltinFunctionType, LambdaType
 
-from typing import cast, Any, List, Optional, Type
+from typing import cast, Any, List, Optional, Sequence, Type
 
 import functools
 
@@ -300,7 +300,7 @@ class Kernel(metaclass=Singleton):
         self.provider.load(LoadRequest(name=name, version=version, tarball=tarball))
 
     def invokeBinScript(
-        self, pkgname: str, script: str, args: Optional[List[Any]] = None
+        self, pkgname: str, script: str, args: Optional[Sequence[str]] = None
     ) -> InvokeScriptResponse:
         if args is None:
             args = []

packages/@jsii/python-runtime/src/jsii/_kernel/providers/process.py
@@ -239,6 +239,7 @@ class _NodeProcess:
         return resources[jsii._embedded.jsii.ENTRYPOINT]
 
     def _next_message(self) -> Mapping[Any, Any]:
+        assert self._process.stdout is not None
         return json.loads(self._process.stdout.readline(), object_hook=ohook)
 
     def start(self):
@@ -278,6 +279,7 @@ class _NodeProcess:
         # This process is closing already, un-registering the hook to not fire twice
         atexit.unregister(self.stop)
 
+        assert self._process.stdin is not None
         if not self._process.stdin.closed:
             self._process.stdin.write(b'{"exit":0}\n')
             # Close the process' STDIN, singaling we are done with it
@@ -311,6 +313,7 @@ class _NodeProcess:
         data = json.dumps(req_dict, default=jdefault).encode("utf8")
 
         # Send our data, ensure that it is framed with a trailing \n
+        assert self._process.stdin is not None
         self._process.stdin.write(b"%b\n" % (data,))
         self._process.stdin.flush()
 

packages/@jsii/python-runtime/src/jsii/_reference_map.py
@@ -91,7 +91,9 @@ class _ReferenceMap:
 
             python_props = {
                 python_name: kernel.get(remote_struct, jsii_name)
-                for python_name, jsii_name in python_jsii_mapping(data_type).items()
+                for python_name, jsii_name in (
+                    python_jsii_mapping(data_type) or {}
+                ).items()
             }
 
             return data_type(**python_props)
@@ -117,8 +119,8 @@ class _ReferenceMap:
                 return struct(
                     **{
                         python_name: kernel.get(remote_struct, jsii_name)
-                        for python_name, jsii_name in python_jsii_mapping(
-                            struct
+                        for python_name, jsii_name in (
+                            python_jsii_mapping(struct) or {}
                         ).items()
                     }
                 )
@@ -152,7 +154,7 @@ class InterfaceDynamicProxy(object):
 
     def __setattr__(self, name, value):
         if name == "_delegates":
-            return super.__setattr__(self, name, value)
+            return super().__setattr__(name, value)
         for delegate in self._delegates:
             if hasattr(delegate, name):
                 return setattr(delegate, name, value)
@@ -164,7 +166,7 @@ def new_combined_struct(structs: Iterable[Type]) -> Type:
     label = " + ".join(struct.__name__ for struct in structs)
 
     def __init__(self, **kwargs):
-        self._values: Mapping[str, Any] = kwargs
+        self._values = kwargs
 
     def __eq__(self, rhs: Any) -> bool:
         return isinstance(rhs, self.__class__) and rhs._values == self._values

commit 0cf500864c918a799505625b08025293431031c7

fix(python): bin script python support, test argument passing (#3762)
Corrects multiple issues with running `bin` scripts from python.

These were discovered in the context of this `projen` issue: https://github.com/projen/projen/issues/2103

 - Corrects broken argument marshaling to binary scripts introduced in #3694
 - Exposes exit code and stderr from script execution instead of failing silently

Additionally, adds more robust test coverage of passing arguments to `bin` scripts.

---

By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].

[Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

.all-contributorsrc
@@ -1466,6 +1466,15 @@
       "contributions": [
         "bug"
       ]
+    },
+    {
+      "login": "jmalins",
+      "name": "Jeff Malins",
+      "avatar_url": "https://avatars.githubusercontent.com/u/2001356?v=4",
+      "profile": "https://github.com/jmalins",
+      "contributions": [
+        "code"
+      ]
     }
   ],
   "repoType": "github",

packages/@jsii/python-runtime/src/jsii/_runtime.py
@@ -1,9 +1,10 @@
 import abc
 import os
+import sys
 
 import attr
 
-from typing import cast, Any, Callable, List, Optional, Mapping, Type, TypeVar
+from typing import Sequence, cast, Any, Callable, List, Optional, Mapping, Type, TypeVar
 
 from . import _reference_map
 from ._compat import importlib_resources
@@ -47,10 +48,23 @@ class JSIIAssembly:
 
     @classmethod
     def invokeBinScript(
-        cls, pkgname: str, script: str, *args: str, _kernel=kernel
-    ) -> None:
+        cls,
+        pkgname: str,
+        script: str,
+        args: Optional[Sequence[str]] = None,
+        _kernel=kernel,
+    ) -> int:
+        if args is None:
+            args = []
+
         response = _kernel.invokeBinScript(pkgname, script, args)
-        print(response.stdout)
+        if response.stdout:
+            print(response.stdout)
+
+        if response.stderr:
+            print(response.stderr, file=sys.stderr)
+
+        return response.status
 
 
 class JSIIMeta(_ClassPropertyMeta, type):

packages/jsii-calc/bin/run.ts
@@ -3,3 +3,8 @@
 /* eslint-disable no-console */
 
 console.info('Hello World!');
+
+const args = process.argv.slice(2);
+if (args.length > 0) {
+  console.info(`  arguments: ${args.join(', ')}`);
+}

packages/jsii-pacmak/lib/targets/python.ts
@@ -1792,7 +1792,7 @@ class PythonModule implements PythonType {
           code.line();
           emitList(
             code,
-            '__jsii_assembly__.invokeBinScript(',
+            'exit_code = __jsii_assembly__.invokeBinScript(',
             [
               JSON.stringify(this.assembly.name),
               JSON.stringify(name),
@@ -1800,6 +1800,7 @@ class PythonModule implements PythonType {
             ],
             ')',
           );
+          code.line('exit(exit_code)');
           code.closeFile(script_file);
           scripts.push(script_file.replace(/\\/g, '/'));
         }

packages/@jsii/kernel/src/kernel.test.ts
@@ -2144,10 +2144,27 @@ defineTest('invokeBinScript() return output', (sandbox) => {
     script: 'calc',
   });
 
-  expect(result.stdout).toEqual('Hello World!\n');
-  expect(result.stderr).toEqual('');
-  expect(result.status).toEqual(0);
-  expect(result.signal).toBeNull();
+  expect(result).toMatchObject<api.InvokeScriptResponse>({
+    status: 0,
+    stdout: 'Hello World!\n',
+    stderr: '',
+    signal: null,
+  });
+});
+
+defineTest('invokeBinScript() accepts arguments', (sandbox) => {
+  const result = sandbox.invokeBinScript({
+    assembly: 'jsii-calc',
+    script: 'calc',
+    args: ['arg1', 'arg2'],
+  });
+
+  expect(result).toMatchObject<api.InvokeScriptResponse>({
+    status: 0,
+    stdout: 'Hello World!\n  arguments: arg1, arg2\n',
+    stderr: '',
+    signal: null,
+  });
 });
 
 // =================================================================================================

packages/@jsii/python-runtime/tests/test_invoke_bin.py
@@ -1,0 +1,38 @@
+import platform
+import subprocess
+import sys
+import pytest
+
+
+class TestInvokeBinScript:
+    @pytest.mark.skipif(
+        platform.system() == "Windows",
+        reason="jsii-pacmak does not generate windows scripts",
+    )
+    def test_invoke_script(self) -> None:
+        result = run_script("calc")
+
+        assert result.returncode == 0
+        assert result.stdout == b"Hello World!\n\n"
+        assert result.stderr == b""
+
+    @pytest.mark.skipif(
+        platform.system() == "Windows",
+        reason="jsii-pacmak does not generate windows scripts",
+    )
+    def test_invoke_script_with_args(self) -> None:
+        result = run_script("calc", "arg1", "arg2")
+
+        assert result.returncode == 0
+        assert result.stdout == b"Hello World!\n  arguments: arg1, arg2\n\n"
+        assert result.stderr == b""
+
+
+def run_script(script_name: str, *args: str) -> subprocess.CompletedProcess:
+    if platform.system() == "Windows":
+        # currently not used, the calling semantics have not been defined for bin scripts on windows
+        script_path = f".env\\Scripts\\{script_name}"
+        return subprocess.run([sys.executable, script_path, *args], capture_output=True)
+    else:
+        script_path = f".env/bin/{script_name}"
+        return subprocess.run([script_path, *args], capture_output=True)

packages/jsii-pacmak/test/generated-code/__snapshots__/target-python.test.js.snap
@@ -11297,7 +11297,8 @@ __jsii_assembly__ = jsii.JSIIAssembly.load(
     "jsii-calc", "3.20.120", "jsii_calc", "jsii-calc@3.20.120.jsii.tgz"
 )
 
-__jsii_assembly__.invokeBinScript("jsii-calc", "calc", sys.argv[1:])
+exit_code = __jsii_assembly__.invokeBinScript("jsii-calc", "calc", sys.argv[1:])
+exit(exit_code)
 
 `;
 