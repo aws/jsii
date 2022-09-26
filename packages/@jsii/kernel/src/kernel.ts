aws/jsii

commit 4c2dcd5562b2abdfb2a454cb0ee487c4a0533e12

feat(kernel): experimental runtime package cache (#3724)
Adds an experimental (hence opt-in) feature that caches the contents of
loaded libraries in a directory that persists between executions, in
order to spare the time it takes to extract the tarballs.

When this feature is enabled, packages present in the cache will be used
as-is (i.e: they are not checked for tampering) instead of being
extracted from the tarball. The cache is keyed on:
- The hash of the tarball
- The name of the library
- The version of the library

Objects in the cache will expire if they are not used for 30 days, and
are subsequently removed from disk (this avoids a cache growing
extremely large over time).

In order to enable the feature, the following environment variables are
used:
- `JSII_RUNTIME_PACKAGE_CACHE` must be set to `enabled` in order for the
  package cache to be active at all;
- `JSII_RUNTIME_PACKAGE_CACHE_ROOT` can be used to change which
  directory is used as a cache root. It defaults to:
  * On MacOS: `$HOME/Library/Caches/com.amazonaws.jsii`
  * On Linux: `$HOME/.cache/aws/jsii/package-cache`
  * On Windows: `%LOCALAPPDATA%\AWS\jsii\package-cache`
  * On other platforms: `$TMP/aws-jsii-package-cache`
- `JSII_RUNTIME_PACKAGE_CACHE_TTL` can be used to change the default
  time entries will remain in cache before expiring if they are not
  used. This defaults to 30 days, and the value is expressed in days.
  Set to `0` to immediately expire all the cache's content.

When troubleshooting load performance, it is possible to obtain timing
data for some critical parts of the library load process within the jsii
kernel by setting `JSII_DEBUG_TIMING` environment variable.

Related to #3389



---

By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].

[Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

packages/@jsii/kernel/src/kernel.ts
@@ -1,23 +1,29 @@
 import * as spec from '@jsii/spec';
 import { loadAssemblyFromPath } from '@jsii/spec';
 import * as cp from 'child_process';
+import { renameSync } from 'fs';
 import * as fs from 'fs-extra';
 import { createRequire } from 'module';
 import * as os from 'os';
 import * as path from 'path';
-import * as tar from 'tar';
 
 import * as api from './api';
 import { TOKEN_REF } from './api';
+import { link } from './link';
 import { jsiiTypeFqn, ObjectTable, tagJsiiConstructor } from './objects';
 import * as onExit from './on-exit';
 import * as wire from './serialization';
+import * as tar from './tar-cache';
 
 export class Kernel {
   /**
    * Set to true for verbose debugging.
    */
   public traceEnabled = false;
+  /**
+   * Set to true for timing data to be emitted.
+   */
+  public debugTimingEnabled = false;
 
   private readonly assemblies = new Map<string, Assembly>();
   private readonly objects = new ObjectTable(this._typeInfoForFqn.bind(this));
@@ -40,6 +46,13 @@ export class Kernel {
   public constructor(public callbackHandler: (callback: api.Callback) => any) {}
 
   public load(req: api.LoadRequest): api.LoadResponse {
+    return this._debugTime(
+      () => this._load(req),
+      `load(${JSON.stringify(req, null, 2)})`,
+    );
+  }
+
+  private _load(req: api.LoadRequest): api.LoadResponse {
     this._debug('load', req);
 
     if ('assembly' in req) {
@@ -74,21 +87,41 @@ export class Kernel {
       };
     }
 
-    // Create the install directory (there may be several path components for @scoped/packages)
-    fs.mkdirpSync(packageDir);
-
     // Force umask to have npm-install-like permissions
     const originalUmask = process.umask(0o022);
     try {
       // untar the archive to its final location
-      tar.extract({
-        cwd: packageDir,
-        file: req.tarball,
-        strict: true,
-        strip: 1, // Removes the 'package/' path element from entries
-        sync: true,
-        unlink: true,
-      });
+      const { path: extractedTo, cache } = this._debugTime(
+        () =>
+          tar.extract(
+            req.tarball,
+            {
+              strict: true,
+              strip: 1, // Removes the 'package/' path element from entries
+              unlink: true,
+            },
+            req.name,
+            req.version,
+          ),
+        `tar.extract(${req.tarball}) => ${packageDir}`,
+      );
+
+      // Create the install directory (there may be several path components for @scoped/packages)
+      fs.mkdirSync(path.dirname(packageDir), { recursive: true });
+      if (cache != null) {
+        this._debug(
+          `Package cache enabled, extraction resulted in a cache ${cache}`,
+        );
+
+        // Link the package into place.
+        this._debugTime(
+          () => link(extractedTo, packageDir),
+          `link(${extractedTo}, ${packageDir})`,
+        );
+      } else {
+        // This is not from cache, so we move it around instead of copying.
+        renameSync(extractedTo, packageDir);
+      }
     } finally {
       // Reset umask to the initial value
       process.umask(originalUmask);
@@ -97,15 +130,26 @@ export class Kernel {
     // read .jsii metadata from the root of the package
     let assmSpec;
     try {
-      assmSpec = loadAssemblyFromPath(packageDir);
+      assmSpec = this._debugTime(
+        () => loadAssemblyFromPath(packageDir),
+        `loadAssemblyFromPath(${packageDir})`,
+      );
     } catch (e: any) {
       throw new Error(`Error for package tarball ${req.tarball}: ${e.message}`);
     }
 
     // load the module and capture its closure
-    const closure = this.require!(packageDir);
+    const closure = this._debugTime(
+      () => this.require!(packageDir),
+      `require(${packageDir})`,
+    );
     const assm = new Assembly(assmSpec, closure);
-    this._addAssembly(assm);
+    this._debugTime(
+      () => this._addAssembly(assm),
+      `registerAssembly({ name: ${assm.metadata.name}, types: ${
+        Object.keys(assm.metadata.types ?? {}).length
+      } })`,
+    );
 
     return {
       assembly: assmSpec.name,
@@ -511,7 +555,7 @@ export class Kernel {
         case spec.TypeKind.Class:
         case spec.TypeKind.Enum:
           const constructor = this._findSymbol(fqn);
-          tagJsiiConstructor(constructor, fqn);
+          tagJsiiConstructor(constructor, fqn, assm.metadata.version);
       }
     }
   }
@@ -1209,6 +1253,20 @@ export class Kernel {
   private _debug(...args: any[]) {
     if (this.traceEnabled) {
       console.error('[@jsii/kernel]', ...args);
+    }
+  }
+
+  private _debugTime<T>(cb: () => T, label: string): T {
+    const fullLabel = `[@jsii/kernel:timing] ${label}`;
+    if (this.debugTimingEnabled) {
+      console.time(fullLabel);
+    }
+    try {
+      return cb();
+    } finally {
+      if (this.debugTimingEnabled) {
+        console.timeEnd(fullLabel);
+      }
     }
   }
 

packages/@jsii/kernel/src/tar-cache/index.ts
@@ -1,0 +1,119 @@
+import { mkdirSync, mkdtempSync, renameSync, rmSync } from 'fs';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import * as tar from 'tar';
+
+import { DiskCache } from '../disk-cache';
+import { defaultCacheRoot } from './default-cache-root';
+
+export type ExtractOptions = Omit<
+  tar.ExtractOptions & tar.FileOptions,
+  'file' | 'cwd'
+>;
+
+export interface ExtractResult {
+  /**
+   * The path in which the extracted files are located
+   */
+  readonly path: string;
+
+  /**
+   * When `'hit'`, the data was already present in cache and was returned from
+   * cache.
+   *
+   * When `'miss'`, the data was extracted into the caache and returned from
+   * cache.
+   *
+   * When `undefined`, the cache is not enabled.
+   */
+  readonly cache?: 'hit' | 'miss';
+}
+
+let packageCacheEnabled =
+  process.env.JSII_RUNTIME_PACKAGE_CACHE?.toLocaleUpperCase() === 'enabled';
+
+/**
+ * Extracts the content of a tarball, possibly caching it on disk.
+ *
+ * @param file     is the path to the tarball to be extracted.
+ * @param options  are options to pass to `tar.extract`
+ * @param comments are included in the cache key, when caching is enabled.
+ *
+ * @returns the result of the extraction.
+ */
+export function extract(
+  file: string,
+  options: ExtractOptions,
+  ...comments: readonly string[]
+): ExtractResult {
+  return (packageCacheEnabled ? extractToCache : extractToTemporary)(
+    file,
+    options,
+    ...comments,
+  );
+}
+
+function extractToCache(
+  file: string,
+  options: ExtractOptions = {},
+  ...comments: readonly string[]
+): { path: string; cache: 'hit' | 'miss' } {
+  const cacheRoot =
+    process.env.JSII_RUNTIME_PACKAGE_CACHE_ROOT ?? defaultCacheRoot();
+  const cache = DiskCache.inDirectory(cacheRoot);
+
+  const entry = cache.entryFor(file, ...comments);
+  return entry.lock((lock) => {
+    let cache: 'hit' | 'miss' = 'hit';
+    if (!entry.pathExists) {
+      const tmpPath = `${entry.path}.tmp`;
+      mkdirSync(tmpPath, { recursive: true });
+      try {
+        untarInto({
+          ...options,
+          cwd: tmpPath,
+          file,
+        });
+        renameSync(tmpPath, entry.path);
+      } catch (error) {
+        rmSync(entry.path, { force: true, recursive: true });
+        throw error;
+      }
+      cache = 'miss';
+    }
+    lock.touch();
+    return { path: entry.path, cache };
+  });
+}
+
+function extractToTemporary(
+  file: string,
+  options: ExtractOptions = {},
+): { path: string } {
+  const path = mkdtempSync(join(tmpdir(), 'jsii-runtime-untar-'));
+
+  untarInto({ ...options, cwd: path, file });
+
+  return { path };
+}
+
+function untarInto(
+  options: tar.ExtractOptions & tar.FileOptions & { cwd: string },
+) {
+  try {
+    tar.extract({ ...options, sync: true });
+  } catch (error) {
+    rmSync(options.cwd, { force: true, recursive: true });
+    throw error;
+  }
+}
+
+/** @internal */
+export function getPackageCacheEnabled(): boolean {
+  return packageCacheEnabled;
+}
+
+/** @internal */
+export function setPackageCacheEnabled(value: boolean) {
+  packageCacheEnabled = value;
+}

packages/@jsii/kernel/src/disk-cache/index.ts
@@ -1,0 +1,1 @@
+export * from './disk-cache';

packages/@jsii/kernel/src/disk-cache/digest-file.ts
@@ -1,0 +1,27 @@
+import { createHash } from 'crypto';
+import { openSync, readSync, closeSync } from 'fs';
+
+const ALGORITHM = 'sha256';
+
+export function digestFile(
+  path: string,
+  ...comments: readonly string[]
+): Buffer {
+  const hash = createHash(ALGORITHM);
+
+  const buffer = Buffer.alloc(16_384);
+  const fd = openSync(path, 'r');
+  try {
+    let bytesRead = 0;
+    while ((bytesRead = readSync(fd, buffer)) > 0) {
+      hash.update(buffer.slice(0, bytesRead));
+    }
+    for (const comment of comments) {
+      hash.update('\0');
+      hash.update(comment);
+    }
+    return hash.digest();
+  } finally {
+    closeSync(fd);
+  }
+}

packages/@jsii/kernel/src/disk-cache/disk-cache.ts
@@ -1,0 +1,244 @@
+import {
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFileSync,
+  realpathSync,
+  rmdirSync,
+  rmSync,
+  statSync,
+  utimesSync,
+  writeFileSync,
+} from 'fs';
+import { lockSync, unlockSync } from 'lockfile';
+import { dirname, join } from 'path';
+
+import { digestFile } from './digest-file';
+
+const MARKER_FILE_NAME = '.jsii-runtime-package-cache';
+
+const ONE_DAY_IN_MS = 86_400_000;
+const PRUNE_AFTER_MILLISECONDS = process.env.JSII_RUNTIME_PACKAGE_CACHE_TTL
+  ? parseInt(process.env.JSII_RUNTIME_PACKAGE_CACHE_TTL, 10) * ONE_DAY_IN_MS
+  : 30 * ONE_DAY_IN_MS;
+
+export class DiskCache {
+  private static readonly CACHE = new Map<string, DiskCache>();
+
+  public static inDirectory(path: string): DiskCache {
+    const didCreate = mkdirSync(path, { recursive: true }) != null;
+    if (didCreate && process.platform === 'darwin') {
+      // Mark the directories for no iCloud sync, no Spotlight indexing, no TimeMachine backup
+      // @see https://michaelbach.de/2019/03/19/MacOS-nosync-noindex-nobackup.html
+      writeFileSync(join(path, '.nobackup'), '');
+      writeFileSync(join(path, '.noindex'), '');
+      writeFileSync(join(path, '.nosync'), '');
+    }
+
+    path = realpathSync(path);
+    if (!this.CACHE.has(path)) {
+      this.CACHE.set(path, new DiskCache(path));
+    }
+    return this.CACHE.get(path)!;
+  }
+
+  readonly #root: string;
+
+  private constructor(root: string) {
+    this.#root = root;
+    process.once('beforeExit', () => this.pruneExpiredEntries());
+  }
+
+  public entry(...key: readonly string[]): Entry {
+    if (key.length === 0) {
+      throw new Error(`Cache entry key must contain at least 1 element!`);
+    }
+
+    return new Entry(
+      join(
+        this.#root,
+        ...key.flatMap((s) =>
+          s
+            .replace(/[^@a-z0-9_.\\/-]+/g, '_')
+            .split(/[\\/]+/)
+            .map((ss) => {
+              if (ss === '..') {
+                throw new Error(
+                  `A cache entry key cannot contain a '..' path segment! (${s})`,
+                );
+              }
+              return ss;
+            }),
+        ),
+      ),
+    );
+  }
+
+  public entryFor(path: string, ...comments: readonly string[]): Entry {
+    const rawDigest = digestFile(path, ...comments);
+    return this.entry(...comments, rawDigest.toString('hex'));
+  }
+
+  public pruneExpiredEntries() {
+    const cutOff = new Date(Date.now() - PRUNE_AFTER_MILLISECONDS);
+    for (const entry of this.entries()) {
+      if (entry.atime < cutOff) {
+        entry.lock((lockedEntry) => {
+          // Check again in case it's been accessed which we waited for the lock...
+          if (entry.atime > cutOff) {
+            return;
+          }
+          lockedEntry.delete();
+        });
+      }
+    }
+
+    for (const dir of directoriesUnder(this.#root, true)) {
+      if (process.platform === 'darwin') {
+        try {
+          rmSync(join(dir, '.DS_Store'), { force: true });
+        } catch {
+          // Ignore errors...
+        }
+      }
+      if (readdirSync(dir).length === 0) {
+        try {
+          rmdirSync(dir);
+        } catch {
+          // Ignore errors, directory may no longer be empty...
+        }
+      }
+    }
+  }
+
+  private *entries(): Generator<Entry, void, void> {
+    yield* inDirectory(this.#root);
+
+    function* inDirectory(dir: string): Generator<Entry, void, void> {
+      if (existsSync(join(dir, MARKER_FILE_NAME))) {
+        return yield new Entry(dir);
+      }
+      for (const file of directoriesUnder(dir)) {
+        yield* inDirectory(file);
+      }
+    }
+  }
+}
+
+export class Entry {
+  public constructor(public readonly path: string) {}
+
+  public get atime(): Date {
+    try {
+      const stat = statSync(this.markerFile);
+      return stat.atime;
+    } catch (err: any) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+      return new Date(0);
+    }
+  }
+
+  public get pathExists() {
+    return existsSync(this.path);
+  }
+
+  private get lockFile(): string {
+    return `${this.path}.lock`;
+  }
+
+  private get markerFile(): string {
+    return join(this.path, MARKER_FILE_NAME);
+  }
+
+  public lock<T>(cb: (entry: LockedEntry) => T): T {
+    mkdirSync(dirname(this.path), { recursive: true });
+    lockSync(this.lockFile, { retries: 12, stale: 5_000 });
+    let disposed = false;
+    try {
+      return cb({
+        delete: () => {
+          if (disposed) {
+            throw new Error(
+              `Cannot delete ${this.path} once the lock block was returned!`,
+            );
+          }
+          rmSync(this.path, { force: true, recursive: true });
+        },
+        write: (name, content) => {
+          if (disposed) {
+            throw new Error(
+              `Cannot write ${join(
+                this.path,
+                name,
+              )} once the lock block was returned!`,
+            );
+          }
+
+          mkdirSync(dirname(join(this.path, name)), { recursive: true });
+          writeFileSync(join(this.path, name), content);
+        },
+        touch: () => {
+          if (disposed) {
+            throw new Error(
+              `Cannot touch ${this.path} once the lock block was returned!`,
+            );
+          }
+          if (this.pathExists) {
+            if (existsSync(this.markerFile)) {
+              const now = new Date();
+              utimesSync(this.markerFile, now, now);
+            } else {
+              writeFileSync(this.markerFile, '');
+            }
+          }
+        },
+      });
+    } finally {
+      disposed = true;
+      unlockSync(this.lockFile);
+    }
+  }
+
+  public read(file: string): Buffer | undefined {
+    try {
+      return readFileSync(join(this.path, file));
+    } catch (error: any) {
+      if (error.code === 'ENOENT') {
+        return undefined;
+      }
+      throw error;
+    }
+  }
+}
+
+export interface LockedEntry {
+  delete(): void;
+  write(name: string, data: Buffer): void;
+
+  touch(): void;
+}
+
+function* directoriesUnder(
+  root: string,
+  recursive = false,
+  ignoreErrors = true,
+): Generator<string, void, void> {
+  for (const file of readdirSync(root)) {
+    const path = join(root, file);
+    try {
+      const stat = statSync(path);
+      if (stat.isDirectory()) {
+        if (recursive) {
+          yield* directoriesUnder(path, recursive, ignoreErrors);
+        }
+        yield path;
+      }
+    } catch (error) {
+      if (!ignoreErrors) {
+        throw error;
+      }
+    }
+  }
+}

packages/@jsii/kernel/src/kernel.test.ts
@@ -14,8 +14,11 @@ import {
   WireStruct,
   TOKEN_STRUCT,
 } from './api';
+import { DiskCache } from './disk-cache';
 import { Kernel } from './kernel';
 import { closeRecording, recordInteraction } from './recording';
+import * as tar from './tar-cache';
+import { defaultCacheRoot } from './tar-cache/default-cache-root';
 
 /* eslint-disable require-atomic-updates */
 
@@ -48,6 +51,11 @@ if (recordingOutput) {
   fs.mkdirpSync(recordingOutput);
   console.error(`JSII_RECORD=${recordingOutput}`);
 }
+
+afterAll(() => {
+  // Jest prevents execution of "beforeExit" events.
+  DiskCache.inDirectory(defaultCacheRoot()).pruneExpiredEntries();
+});
 
 function defineTest(
   name: string,
@@ -2147,6 +2155,9 @@ defineTest('invokeBinScript() return output', (sandbox) => {
 const testNames: { [name: string]: boolean } = {};
 
 async function createCalculatorSandbox(name: string) {
+  // Run half the tests with cache, half without cache... so we test both.
+  tar.setPackageCacheEnabled(!tar.getPackageCacheEnabled());
+
   if (name in testNames) {
     throw new Error(`Duplicate test name: ${name}`);
   }

packages/@jsii/kernel/src/link.ts
@@ -1,0 +1,26 @@
+import { copyFileSync, linkSync, mkdirSync, readdirSync, statSync } from 'fs';
+import { join } from 'path';
+
+/**
+ * Creates directories containing hard links if possible, and falls back on
+ * copy otherwise.
+ *
+ * @param existing    is the original file or directory to link.
+ * @param destination is the nbew file or directory to create.
+ */
+export function link(existing: string, destination: string): void {
+  const stat = statSync(existing);
+  if (!stat.isDirectory()) {
+    try {
+      linkSync(existing, destination);
+    } catch {
+      copyFileSync(existing, destination);
+    }
+    return;
+  }
+
+  mkdirSync(destination, { recursive: true });
+  for (const file of readdirSync(existing)) {
+    link(join(existing, file), join(destination, file));
+  }
+}

packages/@jsii/kernel/src/objects.ts
@@ -16,7 +16,16 @@ const IFACES_SYMBOL = Symbol.for('$__jsii__interfaces__$');
 /**
  * Symbol we use to tag the constructor of a JSII class
  */
-const JSII_SYMBOL = Symbol.for('__jsii__');
+const JSII_RTTI_SYMBOL = Symbol.for('jsii.rtti');
+
+interface ManagedConstructor {
+  readonly [JSII_RTTI_SYMBOL]: {
+    readonly fqn: string;
+    readonly version: string;
+  };
+}
+
+type MaybeManagedConstructor = Partial<ManagedConstructor>;
 
 /**
  * Get the JSII fqn for an object (if available)
@@ -26,7 +35,7 @@ const JSII_SYMBOL = Symbol.for('__jsii__');
  * information.
  */
 export function jsiiTypeFqn(obj: any): string | undefined {
-  return obj.constructor[JSII_SYMBOL]?.fqn;
+  return (obj.constructor as MaybeManagedConstructor)[JSII_RTTI_SYMBOL]?.fqn;
 }
 
 /**
@@ -86,12 +95,19 @@ function tagObject(obj: unknown, objid: string, interfaces?: string[]) {
 /**
  * Set the JSII FQN for classes produced by a given constructor
  */
-export function tagJsiiConstructor(constructor: any, fqn: string) {
-  Object.defineProperty(constructor, JSII_SYMBOL, {
+export function tagJsiiConstructor(
+  constructor: any,
+  fqn: string,
+  version: string,
+) {
+  if (Object.prototype.hasOwnProperty.call(constructor, JSII_RTTI_SYMBOL)) {
+    return;
+  }
+  Object.defineProperty(constructor, JSII_RTTI_SYMBOL, {
     configurable: false,
     enumerable: false,
     writable: false,
-    value: { fqn },
+    value: { fqn, version },
   });
 }
 

commit c98e7c1eb93359c0c7dfd9e750d017fdce0cbc34

fix(kernel): EPERM on rename operation on Windows
Windows does not allow renaming files that are currently open, or
directories that contain open files. When antivirus software is used,
files are open for analysis by the A/V software, making it impossible to
rename files too quickly after they've been created.

This was already reported in #992 and addressed, however the issue was
re-introduced in #3724, in which tarballs were extracted into temporary
directories that were then renamed.

Changed the code back to a form taht extracts files directly into their
final place instead of staging via a temporary space, and added comments
warning maintainers about the specific issue being solved here, so that
hopefully the problem does not get re-introduced again in the future.

Fixes #3751

packages/@jsii/kernel/src/kernel.ts
@@ -1,7 +1,6 @@
 import * as spec from '@jsii/spec';
 import { loadAssemblyFromPath } from '@jsii/spec';
 import * as cp from 'child_process';
-import { renameSync } from 'fs';
 import * as fs from 'fs-extra';
 import { createRequire } from 'module';
 import * as os from 'os';
@@ -9,7 +8,6 @@ import * as path from 'path';
 
 import * as api from './api';
 import { TOKEN_REF } from './api';
-import { link } from './link';
 import { jsiiTypeFqn, ObjectTable, tagJsiiConstructor } from './objects';
 import * as onExit from './on-exit';
 import * as wire from './serialization';
@@ -112,11 +110,15 @@ export class Kernel {
     // Force umask to have npm-install-like permissions
     const originalUmask = process.umask(0o022);
     try {
+      // Create the install directory (there may be several path components for @scoped/packages)
+      fs.mkdirSync(path.dirname(packageDir), { recursive: true });
+
       // untar the archive to its final location
-      const { path: extractedTo, cache } = this._debugTime(
+      const { cache } = this._debugTime(
         () =>
           tar.extract(
             req.tarball,
+            packageDir,
             {
               strict: true,
               strip: 1, // Removes the 'package/' path element from entries
@@ -128,21 +130,10 @@ export class Kernel {
         `tar.extract(${req.tarball}) => ${packageDir}`,
       );
 
-      // Create the install directory (there may be several path components for @scoped/packages)
-      fs.mkdirSync(path.dirname(packageDir), { recursive: true });
       if (cache != null) {
         this._debug(
           `Package cache enabled, extraction resulted in a cache ${cache}`,
         );
-
-        // Link the package into place.
-        this._debugTime(
-          () => link(extractedTo, packageDir),
-          `link(${extractedTo}, ${packageDir})`,
-        );
-      } else {
-        // This is not from cache, so we move it around instead of copying.
-        renameSync(extractedTo, packageDir);
       }
     } finally {
       // Reset umask to the initial value

packages/@jsii/kernel/src/tar-cache/index.ts
@@ -1,9 +1,8 @@
-import { mkdirSync, mkdtempSync, renameSync, rmSync } from 'fs';
-import { tmpdir } from 'os';
-import { join } from 'path';
+import { mkdirSync, rmSync } from 'fs';
 import * as tar from 'tar';
 
 import { DiskCache } from '../disk-cache';
+import { link } from '../link';
 import { defaultCacheRoot } from './default-cache-root';
 
 export type ExtractOptions = Omit<
@@ -12,11 +11,6 @@ export type ExtractOptions = Omit<
 >;
 
 export interface ExtractResult {
-  /**
-   * The path in which the extracted files are located
-   */
-  readonly path: string;
-
   /**
    * When `'hit'`, the data was already present in cache and was returned from
    * cache.
@@ -43,38 +37,43 @@ let packageCacheEnabled =
  */
 export function extract(
   file: string,
+  outDir: string,
   options: ExtractOptions,
   ...comments: readonly string[]
 ): ExtractResult {
-  return (packageCacheEnabled ? extractToCache : extractToTemporary)(
+  return (packageCacheEnabled ? extractViaCache : extractToOutDir)(
     file,
+    outDir,
     options,
     ...comments,
   );
 }
 
-function extractToCache(
+function extractViaCache(
   file: string,
+  outDir: string,
   options: ExtractOptions = {},
   ...comments: readonly string[]
-): { path: string; cache: 'hit' | 'miss' } {
+): { cache: 'hit' | 'miss' } {
   const cacheRoot =
     process.env.JSII_RUNTIME_PACKAGE_CACHE_ROOT ?? defaultCacheRoot();
-  const cache = DiskCache.inDirectory(cacheRoot);
+  const dirCache = DiskCache.inDirectory(cacheRoot);
 
-  const entry = cache.entryFor(file, ...comments);
-  return entry.lock((lock) => {
+  const entry = dirCache.entryFor(file, ...comments);
+  const { path, cache } = entry.lock((lock) => {
     let cache: 'hit' | 'miss' = 'hit';
     if (!entry.pathExists) {
-      const tmpPath = `${entry.path}.tmp`;
-      mkdirSync(tmpPath, { recursive: true });
+      // !!!IMPORTANT!!!
+      // Extract directly into the final target directory, as certain antivirus
+      // software configurations on Windows will make a `renameSync` operation
+      // fail with EPERM until the files have been fully analyzed.
+      mkdirSync(entry.path, { recursive: true });
       try {
         untarInto({
           ...options,
-          cwd: tmpPath,
+          cwd: entry.path,
           file,
         });
-        renameSync(tmpPath, entry.path);
       } catch (error) {
         rmSync(entry.path, { force: true, recursive: true });
         throw error;
@@ -84,17 +83,23 @@ function extractToCache(
     lock.touch();
     return { path: entry.path, cache };
   });
+
+  link(path, outDir);
+
+  return { cache };
 }
 
-function extractToTemporary(
+function extractToOutDir(
   file: string,
+  cwd: string,
   options: ExtractOptions = {},
-): { path: string } {
-  const path = mkdtempSync(join(tmpdir(), 'jsii-runtime-untar-'));
-
-  untarInto({ ...options, cwd: path, file });
-
-  return { path };
+): { cache?: undefined } {
+  // !!!IMPORTANT!!!
+  // Extract directly into the final target directory, as certain antivirus
+  // software configurations on Windows will make a `renameSync` operation
+  // fail with EPERM until the files have been fully analyzed.
+  untarInto({ ...options, cwd, file });
+  return {};
 }
 
 function untarInto(