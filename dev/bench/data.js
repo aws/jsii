e921ba7c89da70ea7b8541268c63048c0e7c69fc
github-action-benchmark <github@users.noreply.github.com>
2022-10-24 18:23:34 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for d9e03596d73e6da8e06a8892aaf733ec186e0608

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666632844009,
+  "lastUpdate": 1666635814624,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -8467,6 +8467,44 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 1647915.5676482553,
             "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 75977.19630439996 milliseconds over 20 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9e03596d73e6da8e06a8892aaf733ec186e0608",
+          "message": "chore(deps): Bump golang.org/x/tools from 0.1.12 to 0.2.0 in /packages/@jsii/go-runtime/jsii-runtime-go (#3809)\n\nBumps [golang.org/x/tools](https://github.com/golang/tools) from 0.1.12 to 0.2.0.\n<details>\n<summary>Release notes</summary>\n<p><em>Sourced from <a href=\"https://github.com/golang/tools/releases\">golang.org/x/tools's releases</a>.</em></p>\n<blockquote>\n<h2>gopls/v0.2.0</h2>\n<ul>\n<li>Many improvements to autocompletion. In particular, support for completions of array, slice, map, and function literals (<a href=\"https://github.com/muirdm\"><code>@​muirdm</code></a>).</li>\n<li>A new diff algorithm (<a href=\"https://github.com/sergi/go-diff\">github.com/sergi/go-diff</a>) that improves handling of line endings on different operating systems (<a href=\"https://github.com/ianthehat\"><code>@​ianthehat</code></a>).</li>\n<li>Improved caching and memory usage (<a href=\"https://github.com/stamblerre\"><code>@​stamblerre</code></a>).</li>\n<li>Command-line support for links, suggested fixes, and imports (<a href=\"https://github.com/kalmanb\"><code>@​kalmanb</code></a>).</li>\n<li>Command-line support for references, signature, and symbols (<a href=\"https://github.com/rentziass\"><code>@​rentziass</code></a>).</li>\n<li>Command-line support for rename (<a href=\"https://github.com/hartzell\"><code>@​hartzell</code></a>).</li>\n</ul>\n<h3>Opt-in:</h3>\n<ul>\n<li>Get diagnostics from <a href=\"https://staticcheck.io/\">staticcheck</a> by configuring <code>&quot;staticcheck&quot;: true</code> in your gopls settings (<a href=\"https://github.com/ianthehat\"><code>@​ianthehat</code></a>).</li>\n<li>Get autocompletion of unimported packages and symbols by configuring <code>&quot;completeUnimported&quot;: true</code> in your gopls settings (<a href=\"https://github.com/heschik\"><code>@​heschik</code></a>).</li>\n</ul>\n</blockquote>\n</details>\n<details>\n<summary>Commits</summary>\n<ul>\n<li><a href=\"https://github.com/golang/tools/commit/f112c43328372460f7ac5bc951711609e22b01cc\"><code>f112c43</code></a> go.mod: update golang.org/x dependencies</li>\n<li><a href=\"https://github.com/golang/tools/commit/207f456f2d4057372e2612f699c66598e5cdd5f4\"><code>207f456</code></a> go/internal/gcimporter: bump version number in skew check</li>\n<li><a href=\"https://github.com/golang/tools/commit/65196caeed0001ac363270d53feaf62d9b90873f\"><code>65196ca</code></a> gopls/README.md: fix wording around supported Go versions</li>\n<li><a href=\"https://github.com/golang/tools/commit/61280309af36ad3b716dc3d4a1703b362ff5fc35\"><code>6128030</code></a> gopls/internal: support renaming packages with int. test variants</li>\n<li><a href=\"https://github.com/golang/tools/commit/649df2ea1a9b6e73d1fd5c4dacd95a1be74683d5\"><code>649df2e</code></a> go.mod: mark as requiring -compat 1.16</li>\n<li><a href=\"https://github.com/golang/tools/commit/91311ab3b8fbb39d8d1df146082b6313ec6e2d55\"><code>91311ab</code></a> gopls/internal/lsp/cache: better import path hygiene</li>\n<li><a href=\"https://github.com/golang/tools/commit/9eda97bc2d7c2fa6ed3f23c9511a6cdeb174b4c8\"><code>9eda97b</code></a> go/analysis: enable a test that applies after go list behavior change</li>\n<li><a href=\"https://github.com/golang/tools/commit/b50d7ba6eeb13c2ded0ec2d8535be9170c065059\"><code>b50d7ba</code></a> gopls: minor cleanup of standalone package support</li>\n<li><a href=\"https://github.com/golang/tools/commit/502b93c33e3e519598e1a93f25ea8e4304def4de\"><code>502b93c</code></a> gopls/internal/lsp: tolerate missing end position in RelatedInformation</li>\n<li><a href=\"https://github.com/golang/tools/commit/d67c3ada0900abbc8e2bf23bb224ebf294fa2026\"><code>d67c3ad</code></a> internal/imports: repair warnings from default analyzers</li>\n<li>Additional commits viewable in <a href=\"https://github.com/golang/tools/compare/v0.1.12...v0.2.0\">compare view</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility score](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=golang.org/x/tools&package-manager=go_modules&previous-version=0.1.12&new-version=0.2.0)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't alter it yourself. You can also trigger a rebase manually by commenting `@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits that have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after your CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge and block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating it. You can achieve the same result by closing it manually\n- `@dependabot ignore this major version` will close this PR and stop Dependabot creating any more for this major version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this minor version` will close this PR and stop Dependabot creating any more for this minor version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this dependency` will close this PR and stop Dependabot creating any more for this dependency (unless you reopen the PR or upgrade to it yourself)\n\n\n</details>",
+          "timestamp": "2022-10-24T17:30:25Z",
+          "tree_id": "16b02293864a0acdcc2a0d3ec841b6d5b6510261",
+          "url": "https://github.com/aws/jsii/commit/d9e03596d73e6da8e06a8892aaf733ec186e0608"
+        },
+        "date": 1666635811566,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0",
+            "value": 73668.69841135,
+            "unit": "milliseconds",
+            "range": 132922.85660458266,
+            "extra": "Compile aws-cdk-lib@v2.31.0 averaged 73668.69841135 milliseconds over 20 runs"
+          },
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0 (tsc)",
+            "value": 53546.8535193,
+            "unit": "milliseconds",
+            "range": 687732.4224656799,
+            "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 53546.8535193 milliseconds over 20 runs"
           }
         ]
       }
