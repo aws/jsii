b3178f2df0e237051417db2b73c5549f98ed1abf
github-action-benchmark <github@users.noreply.github.com>
2022-10-24 22:18:17 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for e27ba71634df8a6f618673b7f683cceab2fd454c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666643524517,
+  "lastUpdate": 1666649897613,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -8581,6 +8581,44 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 212890.38933709744,
             "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 50258.46544419999 milliseconds over 20 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e27ba71634df8a6f618673b7f683cceab2fd454c",
+          "message": "chore(deps): Bump github.com/stretchr/testify from 1.8.0 to 1.8.1 in /packages/@jsii/go-runtime/jsii-runtime-go (#3812)\n\nBumps [github.com/stretchr/testify](https://github.com/stretchr/testify) from 1.8.0 to 1.8.1.\n<details>\n<summary>Commits</summary>\n<ul>\n<li><a href=\"https://github.com/stretchr/testify/commit/b747d7c5f853d017ddbc5e623d026d7fc2770a58\"><code>b747d7c</code></a> Bump github.com/stretchr/objx from 0.4.0 to 0.5.0 (<a href=\"https://github-redirect.dependabot.com/stretchr/testify/issues/1283\">#1283</a>)</li>\n<li>See full diff in <a href=\"https://github.com/stretchr/testify/compare/v1.8.0...v1.8.1\">compare view</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility score](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=github.com/stretchr/testify&package-manager=go_modules&previous-version=1.8.0&new-version=1.8.1)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't alter it yourself. You can also trigger a rebase manually by commenting `@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits that have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after your CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge and block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating it. You can achieve the same result by closing it manually\n- `@dependabot ignore this major version` will close this PR and stop Dependabot creating any more for this major version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this minor version` will close this PR and stop Dependabot creating any more for this minor version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this dependency` will close this PR and stop Dependabot creating any more for this dependency (unless you reopen the PR or upgrade to it yourself)\n\n\n</details>",
+          "timestamp": "2022-10-24T21:19:35Z",
+          "tree_id": "77bc5b24ef621b0fdcdde9a2741ebd7abbd5ae52",
+          "url": "https://github.com/aws/jsii/commit/e27ba71634df8a6f618673b7f683cceab2fd454c"
+        },
+        "date": 1666649894907,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0",
+            "value": 81146.6827886,
+            "unit": "milliseconds",
+            "range": 588455.9342173019,
+            "extra": "Compile aws-cdk-lib@v2.31.0 averaged 81146.6827886 milliseconds over 20 runs"
+          },
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0 (tsc)",
+            "value": 60272.796493949994,
+            "unit": "milliseconds",
+            "range": 518612.2192111031,
+            "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 60272.796493949994 milliseconds over 20 runs"
           }
         ]
       }
