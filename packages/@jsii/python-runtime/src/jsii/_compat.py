772f34fd27ad9d18cb617f0ab34d5b4f794dc937
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2023-03-06 14:35:15 +0100

    always load importlib_resources instead of fooling around

--- packages/@jsii/python-runtime/src/jsii/_compat.py
+++ packages/@jsii/python-runtime/src/jsii/_compat.py
@@ -2,10 +2,7 @@
 import sys
 
 
-if sys.version_info < (3, 11):
-    import importlib_resources
-else:
-    import importlib.resources as importlib_resources
+import importlib_resources
 
 
 __all__ = ["importlib_resources"]
