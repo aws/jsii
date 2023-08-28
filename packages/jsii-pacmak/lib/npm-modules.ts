472e622d3fd1c57b63c8671e8cac15365e23f557
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2023-08-28 11:10:57 -0400

    Update packages/jsii-pacmak/lib/npm-modules.ts

--- packages/jsii-pacmak/lib/npm-modules.ts
+++ packages/jsii-pacmak/lib/npm-modules.ts
@@ -135,7 +135,7 @@ async function updateNpmIgnore(
       'Exclude typescript source and config',
       '*.ts',
       'tsconfig.json',
-      '*.tsbuildinfo'
+      '*.tsbuildinfo',
     );
     includePattern(
       'Include javascript files and typescript declarations',
