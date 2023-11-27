6fcf1eb1cd7b1d06590fb283571a5c8099a2b3c3
github-action-benchmark <github@users.noreply.github.com>
2023-11-27 15:53:14 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for 4d36f1b6383d51785b76f898dc35e4f1bea917ab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701097927394,
+  "lastUpdate": 1701100394846,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -24503,6 +24503,44 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 276710.23697105667,
             "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 39019.59062765 milliseconds over 20 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mail@moritzkornher.de",
+            "name": "Momo Kornher",
+            "username": "mrgrain"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4d36f1b6383d51785b76f898dc35e4f1bea917ab",
+          "message": "chore: use Node 18 in yarn upgrade workflow (#4355)\n\nWorkflow currently fails due to the Node version being incompatible with used packages.\n\n\n---\n\nBy submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].\n\n[Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0",
+          "timestamp": "2023-11-27T15:13:12Z",
+          "tree_id": "16677554c40de8bd37bd828a629413f0ae4242cb",
+          "url": "https://github.com/aws/jsii/commit/4d36f1b6383d51785b76f898dc35e4f1bea917ab"
+        },
+        "date": 1701100390203,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0",
+            "value": 52639.04612964997,
+            "unit": "milliseconds",
+            "range": 301218.73096621025,
+            "extra": "Compile aws-cdk-lib@v2.31.0 averaged 52639.04612964997 milliseconds over 20 runs"
+          },
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0 (tsc)",
+            "value": 39728.79857824998,
+            "unit": "milliseconds",
+            "range": 143384.6303983936,
+            "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 39728.79857824998 milliseconds over 20 runs"
           }
         ]
       }
