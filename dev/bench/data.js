b644d72bfd934e27456750122d9db7a4251786ce
github-action-benchmark <github@users.noreply.github.com>
2023-11-28 18:08:42 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for b5802449f4b9292b3396353f1fa3fa768a779cd7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701176759416,
+  "lastUpdate": 1701194922268,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -24655,6 +24655,44 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 90203.56026107469,
             "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 39391.3137651 milliseconds over 20 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mail@moritzkornher.de",
+            "name": "Momo Kornher",
+            "username": "mrgrain"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b5802449f4b9292b3396353f1fa3fa768a779cd7",
+          "message": "feat: deprecate `jsii/superchain:buster-slim-*` images (#4359)\n\njsii now requires Python 3.8\nDebian Buster only ships with Python 3.7 and we recommend users to upgrade to Bullseye based images.\nLet's stop building Buster images.\n\nAlso clarifies our build permutations in the README.\n\n---\n\nBy submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].\n\n[Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0",
+          "timestamp": "2023-11-28T17:28:24Z",
+          "tree_id": "7e4674afd0b2bf7337aecc8cc95d660ae9376674",
+          "url": "https://github.com/aws/jsii/commit/b5802449f4b9292b3396353f1fa3fa768a779cd7"
+        },
+        "date": 1701194919261,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0",
+            "value": 51894.632283650004,
+            "unit": "milliseconds",
+            "range": 1478242.7832046677,
+            "extra": "Compile aws-cdk-lib@v2.31.0 averaged 51894.632283650004 milliseconds over 20 runs"
+          },
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0 (tsc)",
+            "value": 39433.362081500025,
+            "unit": "milliseconds",
+            "range": 121771.8093112257,
+            "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 39433.362081500025 milliseconds over 20 runs"
           }
         ]
       }
