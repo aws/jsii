fc8741ad9313e7fdfff5169b838b2a6ca4698f8f
github-action-benchmark <github@users.noreply.github.com>
2023-12-13 13:02:20 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for c628cc8af2802f919d8cc235e4d619ffaf081126

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702303502798,
+  "lastUpdate": 1702472540576,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -25187,6 +25187,44 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 30276.63995843439,
             "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 39184.85309304999 milliseconds over 20 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c628cc8af2802f919d8cc235e4d619ffaf081126",
+          "message": "chore(deps): Bump golang.org/x/tools from 0.16.0 to 0.16.1 in /packages/@jsii/go-runtime-test/project (#4374)\n\nBumps [golang.org/x/tools](https://github.com/golang/tools) from 0.16.0 to 0.16.1.\n<details>\n<summary>Commits</summary>\n<ul>\n<li><a href=\"https://github.com/golang/tools/commit/2acb2e697b49f179be93652febb89d384f0f09c9\"><code>2acb2e6</code></a> gopls/internal/test/marker: minor clean up of marker test doc</li>\n<li><a href=\"https://github.com/golang/tools/commit/28b92af2866ab2bc225795ba13f5a1ae765ffec5\"><code>28b92af</code></a> internal/typeparams: eliminate remainining compatibility shims</li>\n<li><a href=\"https://github.com/golang/tools/commit/ee35f8ea92b83ca45ebd96714001e9312bfc96a0\"><code>ee35f8e</code></a> gopls/internal/lsp/source: hovering over broken packages is not an error</li>\n<li><a href=\"https://github.com/golang/tools/commit/67611a11a1be5466d372eba4e925d97290929ef4\"><code>67611a1</code></a> internal/typeparams: eliminate type aliases</li>\n<li><a href=\"https://github.com/golang/tools/commit/23c86e8ed69114240a9c50fbacdbbeadb4b63385\"><code>23c86e8</code></a> internal/typeparams: delete const Enabled=true and simplify</li>\n<li><a href=\"https://github.com/golang/tools/commit/e46688f41326d03a6df28607f1b4d170e31b16ad\"><code>e46688f</code></a> gopls/internal/analysis/fillstruct: don't panic with invalid fields</li>\n<li><a href=\"https://github.com/golang/tools/commit/8bd7553f7c595ecbf244343302e1bfe890f1f947\"><code>8bd7553</code></a> gopls/internal/util/goversion: warn about EOL for Go 1.18</li>\n<li><a href=\"https://github.com/golang/tools/commit/bc9cd159c589d9f78867aaeb5c70867c58e24e61\"><code>bc9cd15</code></a> gopls/internal/settings: remove MemoryMode option</li>\n<li><a href=\"https://github.com/golang/tools/commit/bbc30f1f81aa4a8e2b390bdcc5ebb019e90d658e\"><code>bbc30f1</code></a> gopls/protocol: Allow AnnotatedTextEdits</li>\n<li><a href=\"https://github.com/golang/tools/commit/f40889dc8bf08e65329aa3f302ba7af3024d712d\"><code>f40889d</code></a> gopls/internal/analysis/stubmethods: fix OOB panic in fromValueSpec</li>\n<li>Additional commits viewable in <a href=\"https://github.com/golang/tools/compare/v0.16.0...v0.16.1\">compare view</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility score](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=golang.org/x/tools&package-manager=go_modules&previous-version=0.16.0&new-version=0.16.1)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't alter it yourself. You can also trigger a rebase manually by commenting `@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits that have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after your CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge and block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating it. You can achieve the same result by closing it manually\n- `@dependabot show <dependency name> ignore conditions` will show all of the ignore conditions of the specified dependency\n- `@dependabot ignore this major version` will close this PR and stop Dependabot creating any more for this major version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this minor version` will close this PR and stop Dependabot creating any more for this minor version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this dependency` will close this PR and stop Dependabot creating any more for this dependency (unless you reopen the PR or upgrade to it yourself)\n\n\n</details>",
+          "timestamp": "2023-12-13T12:23:43Z",
+          "tree_id": "1cc3d6563e2ac8bda2d97b64d2f7ba09d341aec2",
+          "url": "https://github.com/aws/jsii/commit/c628cc8af2802f919d8cc235e4d619ffaf081126"
+        },
+        "date": 1702472537114,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0",
+            "value": 50858.750001349974,
+            "unit": "milliseconds",
+            "range": 960135.3842691956,
+            "extra": "Compile aws-cdk-lib@v2.31.0 averaged 50858.750001349974 milliseconds over 20 runs"
+          },
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0 (tsc)",
+            "value": 38846.19903864999,
+            "unit": "milliseconds",
+            "range": 37241.222175187584,
+            "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 38846.19903864999 milliseconds over 20 runs"
           }
         ]
       }
