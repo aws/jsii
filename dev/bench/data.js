f116737d60f4e2046abc54bf6a43ddff140ea8eb
github-action-benchmark <github@users.noreply.github.com>
2024-04-23 11:38:08 +0000

    add jsii Benchmark (customSmallerIsBetter) benchmark result for 9edbf46c40121eeecf1c0cd89f1ded5083520c99

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713793483978,
+  "lastUpdate": 1713872288651,
   "repoUrl": "https://github.com/aws/jsii",
   "entries": {
     "jsii Benchmark": [
@@ -28303,6 +28303,44 @@ window.BENCHMARK_DATA = {
             "unit": "milliseconds",
             "range": 118831.72741257124,
             "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 39867.033313899985 milliseconds over 20 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9edbf46c40121eeecf1c0cd89f1ded5083520c99",
+          "message": "chore(deps): Bump tar from 6.2.0 to 6.2.1 (#4478)\n\nBumps [tar](https://github.com/isaacs/node-tar) from 6.2.0 to 6.2.1.\n<details>\n<summary>Commits</summary>\n<ul>\n<li><a href=\"https://github.com/isaacs/node-tar/commit/bef7b1e4ffab822681fea2a9b22187192ed14717\"><code>bef7b1e</code></a> 6.2.1</li>\n<li><a href=\"https://github.com/isaacs/node-tar/commit/fe8cd57da5686f8695415414bda49206a545f7f7\"><code>fe8cd57</code></a> prevent extraction in excessively deep subfolders</li>\n<li><a href=\"https://github.com/isaacs/node-tar/commit/fe7ebfdcede1f8a2e65db12e19ecc4b3a9934648\"><code>fe7ebfd</code></a> remove security.md</li>\n<li>See full diff in <a href=\"https://github.com/isaacs/node-tar/compare/v6.2.0...v6.2.1\">compare view</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility score](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=tar&package-manager=npm_and_yarn&previous-version=6.2.0&new-version=6.2.1)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't alter it yourself. You can also trigger a rebase manually by commenting `@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits that have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after your CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge and block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating it. You can achieve the same result by closing it manually\n- `@dependabot show <dependency name> ignore conditions` will show all of the ignore conditions of the specified dependency\n- `@dependabot ignore this major version` will close this PR and stop Dependabot creating any more for this major version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this minor version` will close this PR and stop Dependabot creating any more for this minor version (unless you reopen the PR or upgrade to it yourself)\n- `@dependabot ignore this dependency` will close this PR and stop Dependabot creating any more for this dependency (unless you reopen the PR or upgrade to it yourself)\nYou can disable automated security fix PRs for this repo from the [Security Alerts page](https://github.com/aws/jsii/network/alerts).\n\n</details>",
+          "timestamp": "2024-04-23T10:58:02Z",
+          "tree_id": "a1445070b0624553d551fbd6722049f359911046",
+          "url": "https://github.com/aws/jsii/commit/9edbf46c40121eeecf1c0cd89f1ded5083520c99"
+        },
+        "date": 1713872284726,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0",
+            "value": 53497.56180984997,
+            "unit": "milliseconds",
+            "range": 659949.4258180013,
+            "extra": "Compile aws-cdk-lib@v2.31.0 averaged 53497.56180984997 milliseconds over 20 runs"
+          },
+          {
+            "name": "Compile aws-cdk-lib@v2.31.0 (tsc)",
+            "value": 40017.01583425,
+            "unit": "milliseconds",
+            "range": 365247.2982717839,
+            "extra": "Compile aws-cdk-lib@v2.31.0 (tsc) averaged 40017.01583425 milliseconds over 20 runs"
           }
         ]
       }
