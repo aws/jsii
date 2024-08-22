a0e88b026706311682b0400ac125256598605657
epolon <epolon@amazon.com>
2024-08-22 10:57:19 +0300

    mid work

--- packages/jsii-pacmak/test/generated-code/harness.ts
+++ packages/jsii-pacmak/test/generated-code/harness.ts
@@ -288,7 +288,7 @@ export async function preparePythonVirtualEnv({
   install = [],
   installOptions = [],
   venvDir = __dirname,
-  systemSitePackages = true,
+  systemSitePackages = false,
 }: {
   install?: readonly string[];
   // some options like `--config-settings` should only be
@@ -378,6 +378,7 @@ async function runMypy(pythonRoot: string): Promise<void> {
   const { env, venvPython } = await preparePythonVirtualEnv();
 
   // Now run mypy on the Python code
+  console.log(`Running mypy with venv: ${venvPython}`);
   return expect(
     shell(
       venvPython,
