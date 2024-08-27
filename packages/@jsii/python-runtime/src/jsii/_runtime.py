5560950059fd609ade6cba548820255690e8fc5b
epolon <epolon@amazon.com>
2024-08-27 09:19:03 +0300

    mid work

--- packages/@jsii/python-runtime/src/jsii/_runtime.py
+++ packages/@jsii/python-runtime/src/jsii/_runtime.py
@@ -169,8 +169,9 @@ def implements(*interfaces: Type[Any]) -> Callable[[T], T]:
         cls.__jsii_type__ = getattr(cls, "__jsii_type__", None)
         cls.__jsii_ifaces__ = getattr(cls, "__jsii_ifaces__", []) + list(interfaces)
         cls.__jsii_proxy_class__ = lambda: getattr(cls, "__jsii_proxy_class__", None)
-        # TODO explain
-        cls.__protocol_attrs__ = getattr(cls, "__jsii_ifaces__", [])
+
+        # https://github.com/agronholm/typeguard/issues/479
+        cls.__protocol_attrs__ = getattr(cls, "__protocol_attrs__", [])
         return cls
 
     return deco
