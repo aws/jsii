1fa88ba4e507e7ffc9396711c7cd24004ea2feb6
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-02-27 04:11:20 -0500

    feat: switch to python 3.9 (#4780)
    Minimum Python version is now 3.9.18.
    
    https://www.python.org/downloads/release/python-3918/
    ---
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

--- packages/jsii-pacmak/lib/targets/python.ts
+++ packages/jsii-pacmak/lib/targets/python.ts
@@ -2164,7 +2164,7 @@ class Package {
       package_dir: { '': 'src' },
       packages: modules.map((m) => m.pythonName),
       package_data: packageData,
-      python_requires: '~=3.8',
+      python_requires: '~=3.9',
       install_requires: [
         `jsii${toPythonVersionRange(`^${VERSION}`)}`,
         'publication>=0.0.3',
@@ -2178,7 +2178,6 @@ class Package {
         'Operating System :: OS Independent',
         'Programming Language :: JavaScript',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
@@ -2300,7 +2299,7 @@ class Package {
     code.line();
     code.line('[tool.pyright]');
     code.line('defineConstant = { DEBUG = true }');
-    code.line('pythonVersion = "3.8"');
+    code.line('pythonVersion = "3.9"');
     code.line('pythonPlatform = "All"');
     code.line('reportSelfClsParameterName = false');
     code.closeFile('pyproject.toml');
