a273710072af0311beba9624d26ba6deb026808a
Timothy Jones <timothy.l.jones@gmail.com>
2025-03-27 23:41:54 +1100

    Simplify fix implementation

--- packages/jsii-pacmak/lib/packaging.ts
+++ packages/jsii-pacmak/lib/packaging.ts
@@ -79,20 +79,17 @@ export class JsiiModule {
       // Take only the last line of npm pack which should contain the
       // tarball name. otherwise, there can be a lot of extra noise there
       // from scripts that emit to STDOUT.
-      const linesSplitByOs = out.trim().split(os.EOL);
-      const lastLineSplitByOs =
-        linesSplitByOs[linesSplitByOs.length - 1].trim();
-      // on Windows, sometimes `npm pack` might print output with
-      // unix line endings, so we have to split again.
-      // This is safe, because '\n' is never a valid character in a file name.
-      const lines = lastLineSplitByOs.split('\n');
+      // Since we are interested in the text *after* the last newline, splitting on '\n' is correct
+      // both on Linux/Mac (EOL = '\n') and Windows (EOL = '\r\n'), and also for UNIX tools running
+      // on Windows (expected EOL = '\r\n', actual EOL = '\n').
+      const lines = out.trim().split('\n');
       const lastLine = lines[lines.length - 1].trim();
 
       if (!lastLine.endsWith('.tgz') && !lastLine.endsWith('.tar.gz')) {
         throw new Error(
           `${packCommand} did not produce tarball from ${
             this.moduleDirectory
-          } into ${tmpdir} (output was ${JSON.stringify(linesSplitByOs)})`,
+          } into ${tmpdir} (output was ${JSON.stringify(lines.map((l) => l.trimEnd()))})`,
         );
       }
 
