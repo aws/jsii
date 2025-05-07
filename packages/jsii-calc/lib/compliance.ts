93e422eedad93763bbac7a8212be8949aa1b632e
Rico Hermans <rix0rrr@gmail.com>
2025-05-07 15:37:59 +0200

    fix(jsii-pacmak): CS0108 <static property> hides inherited  member (#4826)
    C# requires that we explicitly declare when static members are changed in subclasses. In #2373 we added the ability to change the definition of static functions and static read/write properties in this way, but we missed doing the same for static `const` properties.
    
    Give them the same treatment.
    
    (PR includes https://github.com/aws/jsii/pull/4827)
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

--- packages/jsii-calc/lib/compliance.ts
+++ packages/jsii-calc/lib/compliance.ts
@@ -2924,6 +2924,8 @@ export namespace LevelOne {
  * like.
  */
 export class StaticHelloParent {
+  public static readonly PROPERTY: number = 1337;
+
   public static get property(): number {
     return 1337;
   }
@@ -2933,6 +2935,8 @@ export class StaticHelloParent {
   }
 }
 export class StaticHelloChild extends StaticHelloParent {
+  public static readonly PROPERTY: number = 42;
+
   public static get property(): number {
     return 42;
   }
