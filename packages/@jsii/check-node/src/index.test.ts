16d78ce205b6cb4d4a396f450b10e75a50a9db01
Rico Huijbers <rix0rrr@gmail.com>
2025-07-07 10:13:52 +0200

    Disable failing test

--- packages/@jsii/check-node/src/index.test.ts
+++ packages/@jsii/check-node/src/index.test.ts
@@ -11,7 +11,10 @@ test('tested node releases are correctly registered & supported', () => {
 
 // This test is there to ensure house-keeping happens when it should. If we are
 // testing a given node release, it must not have been EOL for over 60 days.
-test(`tested node release (${process.version}) has not been EOL for more than 60 days`, () => {
+
+// @huijbers - This test has been disabled temporarily. Node 18 is EOL, but CDK is still supporting it.
+// Since CDK is still supporting it, we can't have jsii not support it.
+test.skip(`tested node release (${process.version}) has not been EOL for more than 60 days`, () => {
   const { nodeRelease } = NodeRelease.forThisRuntime();
   expect(nodeRelease?.endOfLifeDate?.getTime()).toBeGreaterThan(
     Date.now() - 60 * 86_400_000,
