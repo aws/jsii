1c658be87e221a725046627301013ea7e669693e
Rico Huijbers <rix0rrr@gmail.com>
2025-11-19 15:35:30 +0100

    fix: Typeguard cannot check the protocol
    Versions 3.0 and higher of `typeguard` can't check the jsii protocol
    interfaces and produce a noisy warning.
    
    Because `constructs` hadn't been updated for a while, and it had been
    generated with an old version of `jsii-pacmak` that was forcing
    `typeguard` to `2.13`, no-one had noticed all the changes to `pacmak`
    that try to support a newer version of `typeguard`, because in practice
    it never ended up being installed (all CDK projects will install
    `constructs` which was keeping `typeguard` at an old version).
    
    Revert to that old version, and stick at it until we can fix up
    our generated Python bindings to be more modern.

--- packages/@jsii/python-runtime/setup.py
+++ packages/@jsii/python-runtime/setup.py
@@ -9,6 +9,10 @@ with open("src/jsii/_metadata.json") as fp:
 with open("README.md", encoding="utf8") as fp:
     long_description = fp.read()
 
+# 3.x and newer perform an additional runtime check on interfaces that our interfaces fail.
+# Stick to this old version. <https://github.com/aws/constructs/issues/2825>
+# Defined as a constant to hopefully prevent Dependabot from automatically updating this.
+TYPEGUARD_VERSION = '2.13.3'
 
 setuptools.setup(
     name="jsii",
@@ -34,8 +38,7 @@ setuptools.setup(
         "cattrs>=1.8,<25.4",
         "importlib_resources>=5.2.0",
         "publication>=0.0.3",  # This is used by all generated code.
-        # 4.3.0 is incompatible with generated bindings, see https://github.com/aws/jsii/issues/4658
-        "typeguard>=2.13.3,<4.5.0",  # This is used by all generated code.
+        f"typeguard=={TYPEGUARD_VERSION}",
         "python-dateutil",
         "typing_extensions>=3.8,<5.0",
     ],
